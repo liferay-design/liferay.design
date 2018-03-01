095a70725a24a0456420aa992e70733dd6d20a2c
Paul Hanaoka <paul.hanaoka@liferay.com>
2018-02-28 23:17:29 -0800

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { Route, Redirect, BrowserRouter as Router } from 'react-router-dom'
+import { Route, BrowserRouter as Router } from 'react-router-dom'
 
 import Events from 'pages/events'
 import React from 'react'
@@ -9,9 +9,8 @@ const App = () => (
 			<Route component={Events} path={'/'} />
 		</div>
 	</Router>
-	<Route exact path="/disruptors" 
-		<Redirect to="https://nvite.com/DesignDisruptors/n34we6"/>
-	/>
+	<Route path='/disruptors' component={() => window.location = 'https://nvite.com/DesignDisruptors/n34we6'}/>
+
 )
 
 export default App
