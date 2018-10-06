cca9fe3a7635d4565cafd9029667b761b4bdcc5a
Ryan Garant <ryantgarant@gmail.com>
2018-10-06 14:41:53 -0700

    Convert markdown renderer to mdx renderer

--- src/components/templates/Blueprints/index.js
+++ src/components/templates/Blueprints/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { graphql } from 'gatsby'
 import styles from './styles.module.scss'
 import { cloneDeep, get, set } from 'lodash'
@@ -7,78 +7,46 @@ import { AuthContainer } from 'components/molecules'
 import { ContainerMarkdown, Flex } from 'components/atoms'
 import { PrivatePage } from 'components/templates'
 import { Grid } from 'reakit'
-
-function upsertAtPath(path, value, obj) {
-	obj = cloneDeep(obj)
-	const pathValue = get(obj, path)
-	set(obj, path, { ...pathValue, ...value })
-
-	return obj
-}
+import MDXRenderer from 'gatsby-mdx/mdx-renderer'
 
 const template = `
 	"sidebar main" auto / 18rem 1fr
-`;
-export default class Blueprints extends Component {
-	buildSidebarTree(markdownNodes) {
-		const sidebarTree = markdownNodes.edges.reduce((currentTree, currentValue) => {
-			const slug = currentValue.node.fields.slug
-			const title = currentValue.node.frontmatter.title
+`
 
-			const slugArr = slug
-				.split('/')
-				.filter(slug => slug !== '')
-				.slice(1)
+export default function Blueprints({ data: { allMdx, mdx }, location: { pathname } }) {
+	return (
+		<PrivatePage
+			message="You must be a Liferay Employee to view this page"
+			section="Blueprints"
+		>
+			<div className={styles.sans}>
+				<Grid template={template} className={styles.mainContentWrapper}>
+					<Sidebar path={pathname} tree={buildSidebarTree(allMdx)} />
 
-			const treePath = slugArr.join('.children.')
-			const sidebarItemValue = { title, slug }
+					<ContainerMarkdown>
+						<Flex
+							justify="space-between"
+							align="baseline"
+							className={styles.header}
+						>
+							<h1>{mdx.frontmatter.title}</h1>
 
-			if (!treePath.includes('children')) {
-				sidebarItemValue.firstLevel = true
-			}
+							<AuthContainer />
+						</Flex>
 
-			return upsertAtPath(treePath, sidebarItemValue, currentTree)
-		}, {})
+						<MDXRenderer className={styles.body}>{mdx.code.body}</MDXRenderer>
 
-		return sidebarTree
-	}
-
-	render() {
-		const post = this.props.data.markdownRemark
-		const markdown = this.props.data.allMarkdownRemark
-
-		const sidebarTree = this.buildSidebarTree(markdown)
-
-		return (
-			<PrivatePage
-				message="You must be a Liferay Employee to view this page"
-				section="Blueprints"
-			>
-				<div className={styles.sans}>
-					<Grid template={template} className={styles.mainContentWrapper}>
-						<Sidebar path={this.props.location.pathname} tree={sidebarTree} />
-						<ContainerMarkdown>
-							<Flex justify="space-between" align="baseline" className={styles.header}>
-								<h1>{post.frontmatter.title}</h1>
-								<AuthContainer />
-							</Flex>
-							<div className={styles.body}
-								dangerouslySetInnerHTML={{
-									__html: post.html,
-								}}
-							/>
-							<FooterMarkdown light />
-						</ContainerMarkdown>
-					</Grid>
-				</div>
-			</PrivatePage>
-		)
-	}
+						<FooterMarkdown light />
+					</ContainerMarkdown>
+				</Grid>
+			</div>
+		</PrivatePage>
+	)
 }
 
-export const query = graphql`
+export const pageQuery = graphql`
 	query($slug: String!) {
-		allMarkdownRemark(filter: { fields: { slug: { regex: "/blueprints/" } } }) {
+		allMdx(filter: { fields: { slug: { regex: "/blueprints/" } } }) {
 			totalCount
 			edges {
 				node {
@@ -93,11 +61,44 @@ export const query = graphql`
 			}
 		}
 
-		markdownRemark(fields: { slug: { eq: $slug } }) {
-			html
+		mdx(fields: { slug: { eq: $slug } }) {
 			frontmatter {
 				title
+			}
+			code {
+				body
 			}
 		}
 	}
 `
+
+function upsertAtPath(path, value, obj) {
+	obj = cloneDeep(obj)
+	const pathValue = get(obj, path)
+	set(obj, path, { ...pathValue, ...value })
+
+	return obj
+}
+
+function buildSidebarTree(markdownNodes) {
+	const sidebarTree = markdownNodes.edges.reduce((currentTree, currentValue) => {
+		const slug = currentValue.node.fields.slug
+		const title = currentValue.node.frontmatter.title
+
+		const slugArr = slug
+			.split('/')
+			.filter(slug => slug !== '')
+			.slice(1)
+
+		const treePath = slugArr.join('.children.')
+		const sidebarItemValue = { title, slug }
+
+		if (!treePath.includes('children')) {
+			sidebarItemValue.firstLevel = true
+		}
+
+		return upsertAtPath(treePath, sidebarItemValue, currentTree)
+	}, {})
+
+	return sidebarTree
+}
