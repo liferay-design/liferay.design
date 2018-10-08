bf0704d7a0fe97bc5f1da488d89f449f8a0ca61e
plhnk <paul.hanaoka@gmail.com>
2018-10-08 13:24:04 -0500

    add more Blueprints components, update docs

---
+++ src/components/molecules/AssetCardMultiple/index.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Flex, Text } from 'components/atoms'
+import { ColorField } from 'components/molecules'
+import { Button } from 'reakit'
+import PropTypes from 'prop-types'
+import styles from './styles.module.scss'
+
+export default function AssetCard({ asset, title, preview, link }) {
+	return (
+		<Flex className={styles.container} direction="column" width="100%">
+			<Flex background={name} height="0" padding="0 0 56.25%" width="100%" />
+
+			<Flex direction="column" padding="1rem">
+				<Text type="p" weight="heavy" margin="micro" size="base">
+					{title}
+				</Text>
+
+				<Button href={asset} download={title} />
+
+				<a href={link} target="_blank">
+					Add to Google Drive
+				</a>
+			</Flex>
+		</Flex>
+	)
+}
+
+AssetCard.propTypes = {
+	preview: PropTypes.string.isRequired,
+	asset: PropTypes.string.isRequired,
+	title: PropTypes.string.isRequired,
+	link: PropTypes.string.isRequired,
+}
