2a031f64e1351380c585f3c1bde331ccd5a2b072
Ryan Garant <ryantgarant@gmail.com>
2018-10-12 15:46:53 -0700

    LRIS-33047 Make Sidebar component mmobile friendly

--- src/components/organisms/Team/index.js
+++ src/components/organisms/Team/index.js
@@ -1,22 +1,14 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { CardGrid } from 'components/atoms'
 import { Person } from 'components/molecules'
 import teamMembers from './team_members'
 
-class Team extends Component {
-	state = {
-		teamMembers,
-	}
-
-	render() {
-		return (
-			<CardGrid>
-				{this.state.teamMembers.map(person => (
-					<Person key={person.id} person={person} />
-				))}
-			</CardGrid>
-		)
-	}
+export default function Team() {
+	return (
+		<CardGrid>
+			{teamMembers.map(person => (
+				<Person key={person.id} person={person} />
+			))}
+		</CardGrid>
+	)
 }
-
-export default Team
