9c04a05dc3962a34edef2f17888ba8edded7d553
plhnk <paul.hanaoka@gmail.com>
2018-11-03 23:55:12 -0700

    start blueprints mobile design

--- src/pages/blueprints/index.js
+++ src/pages/blueprints/index.js
@@ -6,17 +6,18 @@ import { Footer, Navbar, Banner } from 'components/organisms'
 import styles from './styles.module.scss'
 import { Card, CardTitle, CardText } from 'react-md'
 import { Grid, Button, styled } from 'reakit'
+import MediaQuery from 'react-responsive'
 
 const WideCard = styled(Card)`
 	display: flex;
 	text-align: left !important;
 `
 
-const cardGrid = `
-	"tlg tlc trc rg" auto
-	"lg m m rg" auto
-	"lg blc brc brg" auto / 3rem 5fr 5fr 3rem
-`
+// const cardGrid = `
+// 	"tlg tlc trc rg" auto
+// 	"lg m m rg" auto
+// 	"lg blc brc brg" auto / 3rem 5fr 5fr 3rem
+// `
 
 const Blueprints = () => (
 	<div className={styles.sans}>
@@ -30,86 +31,108 @@ const Blueprints = () => (
 		/>
 		<div className={styles.background}>
 			<div className={styles.container}>
-				<Grid className={styles.teasers} gap="2rem" template={cardGrid}>
-					<Grid.Item area="tlg" marginTop="-8rem">
-						<Text
-							color="#c4cacb"
-							weight="700"
-							size="12px"
-							type="p"
-							className={styles.vertical}
-						>
-							Explore Blueprints
-						</Text>
-					</Grid.Item>
-					<Grid.Item area="tlc" marginTop="-8rem">
-						<LexiconCard
-							icon="whyBlueprints"
-							title="Why Blueprints"
-							text="A single source of truth for Liferay so that we can go
-									far, together."
-							to="/blueprints/about"
-							cta="Read More"
-						/>
-					</Grid.Item>
-					<Grid.Item area="trc" marginTop="-8rem">
-						<LexiconCard
-							icon="logos"
-							title="Just the Logos, Please"
-							text="We know what you’re looking for, and we respect that.
-								Here it is at the top of the page."
-							to="/blueprints/resources/logo-files"
-							cta="Waffle Time"
-						/>
-					</Grid.Item>
-					<Grid.Item area="m">
-						<LexiconCard
-							wide
-							icon="resources"
-							title="Resources"
-							text="The reason you've come to this site&mdash;logos,
-										colors, fonts, images&mdash;get it all right&nbsp;here!"
-							to="/blueprints/resources"
-							cta="Get 'Em"
-						/>
-					</Grid.Item>
-					<Grid.Item area="blc">
-						<LexiconCard
-							icon="faqs"
-							title="FAQ"
-							text="Have a question? We have frequently given answers."
-							to="/blueprints/faqs"
-							cta="Why is the Liferay Blue?"
-						/>
-					</Grid.Item>
-					<Grid.Item area="brc">
-						<LexiconCard
-							icon="liferayDesign"
-							title="Liferay.Design"
-							text="Shameless plug for the people behind all of this
-									stuff&mdash;see what else we're doing."
-							to="/team"
-							cta="Experience Liferay.Design"
-						/>
-					</Grid.Item>
-					<Grid.Item area="brg" marginTop="2rem">
-						<Link
-							className={styles.social}
-							to="https://github.com/liferay-design"
-						>
-							<Icon fill="white" name="github" />
-						</Link>
-						<Link className={styles.social} to="https://dribbble.com/liferay">
-							<Icon fill="white" name="dribbble" />
-						</Link>
-						<Link
-							className={styles.social}
-							to="https://twitter.com/liferaydesign"
-						>
-							<Icon fill="white" name="twitter" />
-						</Link>
-					</Grid.Item>
-				</Grid>
+				<MediaQuery maxWidth={767}>
+					{matches => {
+						let gridTemplate = matches
+							? `	"tlg" auto // first is mobile
+								"tlc" auto
+								"m" auto
+								"blc" auto
+								"brc" auto / auto-fit, minmax(200px, 1fr)`
+							: `	"tlg tlc trc rg" auto
+								"lg m m rg" auto
+								"lg blc brc brg" auto / 1rem 5fr 5fr 1rem`
+						return (
+							<Grid
+								className={styles.teasers}
+								gap="2rem"
+								template={gridTemplate}
+							>
+								<Grid.Item area="tlg" marginTop="-8rem">
+									<Text
+										color="#c4cacb"
+										weight="700"
+										size="12px"
+										type="p"
+										className={styles.vertical}
+									>
+										Explore Blueprints
+									</Text>
+								</Grid.Item>
+								<Grid.Item area="tlc" marginTop="-8rem">
+									<LexiconCard
+										icon="whyBlueprints"
+										title="Why Blueprints"
+										text="A single source of truth for Liferay so that we can go
+										far, together."
+										to="/blueprints/about"
+										cta="Read More"
+									/>
+								</Grid.Item>
+								<Grid.Item area="trc" marginTop="-8rem">
+									<LexiconCard
+										icon="logos"
+										title="Just the Logos, Please"
+										text="We know what you’re looking for, and we respect that.
+									Here it is at the top of the page."
+										to="/blueprints/resources/logo-files"
+										cta="Waffle Time"
+									/>
+								</Grid.Item>
+								<Grid.Item area="m">
+									<LexiconCard
+										wide
+										icon="resources"
+										title="Resources"
+										text="The reason you've come to this site&mdash;logos,
+											colors, fonts, images&mdash;get it all right&nbsp;here!"
+										to="/blueprints/resources"
+										cta="Get 'Em"
+									/>
+								</Grid.Item>
+								<Grid.Item area="blc">
+									<LexiconCard
+										icon="faqs"
+										title="FAQ"
+										text="Have a question? We have frequently given answers."
+										to="/blueprints/faqs"
+										cta="Why is the Liferay Blue?"
+									/>
+								</Grid.Item>
+								<Grid.Item area="brc">
+									<LexiconCard
+										icon="liferayDesign"
+										title="Liferay.Design"
+										text="Shameless plug for the people behind all of this
+										stuff&mdash;see what else we're doing."
+										to="/team"
+										cta="Experience Liferay.Design"
+									/>
+								</Grid.Item>
+								<Grid.Item area="brg" marginTop="2rem">
+									<Link
+										className={styles.social}
+										to="https://github.com/liferay-design"
+									>
+										<Icon fill="white" name="github" />
+									</Link>
+									<Link
+										className={styles.social}
+										to="https://dribbble.com/liferay"
+									>
+										<Icon fill="white" name="dribbble" />
+									</Link>
+									<Link
+										className={styles.social}
+										to="https://twitter.com/liferaydesign"
+									>
+										<Icon fill="white" name="twitter" />
+									</Link>
+								</Grid.Item>
+							</Grid>
+						)
+					}}
+				</MediaQuery>
 			</div>
 		</div>
 		<div className={styles.billboard}>
