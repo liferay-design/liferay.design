d0125ffb304757b3557a9739821314bd168b5bd5
plhnk <paul.hanaoka@gmail.com>
2018-11-06 13:48:55 -0800

    Guild Handbook page

--- src/pages/handbook/index.js
+++ src/pages/handbook/index.js
@@ -1,43 +1,61 @@
 import React from 'react'
 import { Link, graphql } from 'gatsby'
 import { MainLayout } from 'components/templates'
+import { Footer, Navbar, Banner } from 'components/organisms'
+import { LexiconCard } from 'components/molecules'
+import { Container } from 'components/atoms'
+import { Grid } from 'reakit'
+import styles from './styles.module.scss'
 
-export default ({ data }) => {
-	return (
-		<div>
-			<MainLayout section="Handbook">
-				<h4>{data.allMdx.totalCount} Posts</h4>
+const HandbookGuild = () => (
+	<div className={styles.body}>
+		<Banner
+			headline="Handbook"
+			subtitle="For the UX Design Guild"
+			cta="Get Started"
+			ctaLink="/handbook/overview"
+			section="Handbook"
+			background="url(/images/handbook/guild/ux-guild.svg) center / cover fixed"
+		/>
+		<Container>
+			<Grid className={styles.teasers} gap="2rem">
+				<Grid.Item>
+					<LexiconCard
+						dark
+						icon="crestEmea"
+						fill="primary"
+						title="EMEA"
+						text="Europe, Middle East, and Africa"
+						to="/handbook/emea"
+						cta="Bonjour!"
+					/>
+				</Grid.Item>
+				<Grid.Item>
+					<LexiconCard
+						dark
+						icon="crestLatam"
+						fill="primary"
+						title="LATAM"
+						text="Latin America"
+						to="/handbook/latam"
+						cta="Olá"
+					/>
+				</Grid.Item>
+				<Grid.Item>
+					<LexiconCard
+						dark
+						icon="crestNa"
+						fill="primary"
+						title="NA"
+						text="North America"
+						to="/handbook/na"
+						cta="Yo"
+					/>
+				</Grid.Item>
+			</Grid>
+		</Container>
+		<Footer />
+	</div>
+)
 
-				{data.allMdx.edges.map(({ node }) => (
-					<div key={node.id}>
-						<Link to={node.fields.slug}>
-							<h3>{node.frontmatter.title}</h3>
-
-							<p>{node.excerpt}</p>
-						</Link>
-					</div>
-				))}
-			</MainLayout>
-		</div>
-	)
-}
-
-export const query = graphql`
-	{
-		allMdx(filter: { fileAbsolutePath: { regex: "/(handbook)/(guild)/" } }) {
-			totalCount
-			edges {
-				node {
-					id
-					frontmatter {
-						title
-					}
-					fields {
-						slug
-					}
-					excerpt
-				}
-			}
-		}
-	}
-`
+export default HandbookGuild
