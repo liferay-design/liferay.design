dda26385bd2925376dd223cd046656aed42a7e9d
Phillip Chan <phillip.chan@liferay.com>
2018-11-20 08:10:44 -0800

    Allow lexicon card to be horizontal

--- src/components/molecules/LexiconCard/index.js
+++ src/components/molecules/LexiconCard/index.js
@@ -4,36 +4,59 @@ import PropTypes from 'prop-types'
 import styles from './styles.module.scss'
 import { Flex, Heading, Text, Icon } from 'components/atoms'
 
-const LexiconCard = ({ title, text, cta, to, icon, dark, wide, fill }) => {
+const LexiconCard = ({
+	title,
+	text,
+	cta,
+	to,
+	icon,
+	dark,
+	direction,
+	iconWidth,
+	iconHeight,
+}) => {
+	const directionMap = {
+		vertical: 'column',
+		horizontal: 'row',
+	}
+
+	const textAlignmentMap = {
+		vertical: 'center',
+		horizontal: 'left',
+	}
+
+	const iconWidthMap = {
+		vertical: '100%',
+		horizontal: '50%',
+	}
+
 	return (
 		<Link to={to}>
 			<Flex
-				direction="column"
-				className={`${styles.card} ${dark ? styles.dark : styles.light} ${
-					wide ? styles.wide : ''
-				}`}
+				direction={directionMap[direction]}
+				className={`${styles.card} ${dark ? styles.dark : styles.light}`}
 			>
-				<div className={styles.content}>
-					<Flex align="center" justify="center" className={styles.iconWrapper}>
-						<Icon
-							name={icon}
-							fill={fill}
-							width="6rem"
-							height="6rem"
-							margin="1rem 0 2rem"
-						/>
-					</Flex>
-					<div className={styles.copy}>
-						<Heading level={2} className={styles.title}>
-							{title}
-						</Heading>
-						<Text type="p" size="medium" className={styles.text}>
-							{text}
-						</Text>
-						<Text type="p" size="base" weight="black" className={styles.cta}>
-							{cta}
-						</Text>
-					</div>
+				<Flex align="center" justify="center" width={iconWidthMap[direction]}>
+					<Icon
+						name={icon}
+						width={iconWidth}
+						height={iconHeight}
+						margin="1rem 0 2rem"
+					/>
+				</Flex>
+				<div
+					className={styles.copy}
+					style={{ flexBasis: '50%', textAlign: textAlignmentMap[direction] }}
+				>
+					<Heading level={2} className={styles.title}>
+						{title}
+					</Heading>
+					<Text type="p" size="medium" className={styles.text}>
+						{text}
+					</Text>
+					<Text type="p" size="base" weight="black" className={styles.cta}>
+						{cta}
+					</Text>
 				</div>
 			</Flex>
 		</Link>
@@ -41,12 +64,21 @@ const LexiconCard = ({ title, text, cta, to, icon, dark, wide, fill }) => {
 }
 
 LexiconCard.propTypes = {
-	title: PropTypes.string,
-	text: PropTypes.string,
 	cta: PropTypes.string,
-	to: PropTypes.string,
-	styles: PropTypes.string,
+	direction: PropTypes.string,
 	icon: PropTypes.string,
+	iconWidth: PropTypes.string,
+	iconHeight: PropTypes.string,
+	styles: PropTypes.string,
+	text: PropTypes.string,
+	title: PropTypes.string,
+	to: PropTypes.string,
+}
+
+LexiconCard.defaultProps = {
+	direction: 'vertical',
+	iconWidth: '6rem',
+	iconHeight: '6rem',
 }
 
 export default LexiconCard
