70b2b9fbd6078cff37a7a7ba282c1541baa32a6d
plhnk <paul.hanaoka@gmail.com>
2018-11-27 23:40:46 -0800

    add rel=noopener on links

--- src/components/molecules/SocialIcons/index.js
+++ src/components/molecules/SocialIcons/index.js
@@ -5,27 +5,35 @@ import styles from './styles.module.scss'
 const SocialIcons = () => {
 	return (
 		<Flex className={styles.social} justify="space-between">
-			<a target="_blank" href="https://www.dribbble.com/liferay">
+			<a target="_blank" rel="noopener" href="https://www.dribbble.com/liferay">
 				<Icon name="dribbble" />
 			</a>
 
-			<a target="_blank" href="https://github.com/liferay-design/">
+			<a target="_blank" rel="noopener" href="https://github.com/liferay-design/">
 				<Icon name="github" />
 			</a>
 
-			<a target="_blank" href="https://www.instagram.com/liferay_ux/">
+			<a
+				target="_blank"
+				rel="noopener"
+				href="https://www.instagram.com/liferay_ux/"
+			>
 				<Icon name="instagram" />
 			</a>
 
-			<a target="_blank" href="https://www.linkedin.com/company/83609">
+			<a
+				target="_blank"
+				rel="noopener"
+				href="https://www.linkedin.com/company/83609"
+			>
 				<Icon name="linkedin" />
 			</a>
 
-			<a target="_blank" href="https://www.medium.com/liferaydesign">
+			<a target="_blank" rel="noopener" href="https://www.medium.com/liferaydesign">
 				<Icon name="medium" />
 			</a>
 
-			<a target="_blank" href="https://twitter.com/liferaydesign">
+			<a target="_blank" rel="noopener" href="https://twitter.com/liferaydesign">
 				<Icon name="twitter" />
 			</a>
 		</Flex>
