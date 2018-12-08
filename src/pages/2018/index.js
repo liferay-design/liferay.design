b6031d725479bc31a48a830f0e258375bf7d7196
Abel Hancock <abel.hancock@liferay.com>
2018-12-07 17:54:51 -0800

    add lexicon section and image assets

--- src/pages/2018/index.js
+++ src/pages/2018/index.js
@@ -110,6 +110,15 @@ const anchorLinks = [
         ],
       },
   ];
+  const designerHeadshot = [
+    {
+      start: 'self',
+      duration: '40vh',
+      startOffset: '40vh',
+      properties: [
+      ],
+    },
+  ];
 
 //   constant plx data
   const InView = [
@@ -297,7 +306,7 @@ const TwentyEighteen = () => {
                 </AnchorLink>
                 <AnchorLink href='#designers-hired'>
                     <Plx className={styles.anchorBlock} parallaxData={anchorLinksBlocks}></Plx>
-                    <Plx className={styles.anchorText} parallaxData={anchorLinksText} >Designers Hired</Plx>
+                    <Plx className={styles.anchorText} parallaxData={anchorLinksText} >Big Moments</Plx>
                 </AnchorLink>
                 <AnchorLink href='#dribbble'>
                     <Plx className={styles.anchorBlock} parallaxData={anchorLinksBlocks}></Plx>
@@ -456,7 +465,7 @@ const TwentyEighteen = () => {
         <section id="products-launched" className={styles.productsLaunched}>
             <div className={styles.textMega}>04</div>
             <Plx parallaxData={ PlxUp3 } className={styles.f2}>Products Launched</Plx>
-            <Plx parallaxData={ InView } className={styles.products}>
+            <Plx parallaxData={ InView10 } className={styles.products}>
                 <div className={styles.pSmall}>Analytics Cloud</div>
                 <div className={styles.dotSmall}></div>
                 <div className={styles.pSmall}>Commerce</div>
@@ -466,7 +475,7 @@ const TwentyEighteen = () => {
                 <div className={styles.pSmall}>DXP Cloud</div>
             </Plx>
             <Plx parallaxData={ PlxUp3 } className={styles.f3}>Supporting Releases</Plx>
-            <Plx parallaxData={ InView } className={styles.releases}>
+            <Plx parallaxData={ InView10 } className={styles.releases}>
                 <div className={styles.pSmall}>Customer Portal</div>
                 <div className={styles.dotSmall}></div>
                 <div className={styles.pSmall}>University</div>
@@ -526,7 +535,7 @@ const TwentyEighteen = () => {
         {/* Designers hired */}
         <section id="designers-hired" className={styles.designersHired}>
             <Plx className={styles.title} parallaxData={ PlxUp2 }>
-                <h2 className={styles.f2}>Designers Hired</h2>
+                <h2 className={styles.f2}>Big Moments</h2>
             </Plx>
             <Flex direction="column" justify="space-between" className={styles.timeline}>
                 <div className={styles.line}>
@@ -534,21 +543,21 @@ const TwentyEighteen = () => {
                         <line x1="0.5" y1="0" x2="0.5" y2="100%" stroke="#006DE7" stroke-linejoin="round" stroke-dasharray="1 10"/>
                     </svg>
                 </div>
-                <Plx parallaxData={ designerHired } className={styles.hired}>
+                <Plx tagName="a" href="team/bargaid-duuriimaa" parallaxData={ designerHired } className={styles.hired}>
                     <div className={styles.date}>
                         <div className={styles.pLarge}>Feb</div>
                         <div className={styles.f1}>08</div>
                     </div>
-                    <div className={styles.dot}></div>
+                    <Plx tagName="img" parallaxData={ designerHeadshot } className={styles.designerHeadshot} src="images/headshots/bargaid-duuriimaa.jpg" />
                     <h4 className={styles.pLarge}>Duuriimaa Bargaid <span>&nbsp;&nbsp; Spain</span></h4>
                 </Plx>
 
-                <Plx parallaxData={ designerHired } className={styles.birthray}>
+                <Plx tagName="a" href="team/connolly-ryan" parallaxData={ designerHired } className={styles.birthray}>
                     <div className={styles.date}>
                         <div className={styles.pLarge}>Apr</div>
                         <div className={styles.pLarge}>24</div>
                     </div>
-                    <div className={styles.dot}></div>
+                    <Plx tagName="img" parallaxData={ designerHeadshot } className={styles.designerHeadshot} src="images/headshots/connolly-ryan.jpg" />
                     <h4 className={styles.pSmall}>Ryan Connolly <span>&nbsp;&nbsp; 1st Birthray</span></h4>
                 </Plx>
 
@@ -557,7 +566,7 @@ const TwentyEighteen = () => {
                         <div className={styles.pLarge}>May</div>
                         <div className={styles.f1}>07</div>
                     </div>
-                    <div className={styles.dot}></div>
+                    <Plx tagName="img" parallaxData={ designerHeadshot } className={styles.designerHeadshot} src="images/headshots/-sunglasses.png" />
                     <h4 className={styles.pLarge}>Andras Himmer <span>&nbsp;&nbsp; Hungary</span></h4>
                 </Plx>
 
@@ -566,7 +575,7 @@ const TwentyEighteen = () => {
                         <div className={styles.pLarge}>May</div>
                         <div className={styles.f1}>14</div>
                     </div>
-                    <div className={styles.dot}></div>
+                    <Plx tagName="img" parallaxData={ designerHeadshot } className={styles.designerHeadshot} src="images/headshots/-heart-eyes-cat.png" />
                     <h4 className={styles.pLarge}>Andrea Censi <span>&nbsp;&nbsp; Italy</span></h4>
                 </Plx>
 
@@ -575,7 +584,7 @@ const TwentyEighteen = () => {
                         <div className={styles.pLarge}>Jul</div>
                         <div className={styles.pLarge}>03</div>
                     </div>
-                    <div className={styles.dot}></div>
+                    <Plx tagName="img" parallaxData={ designerHeadshot } className={styles.designerHeadshot} src="images/headshots/oliveira-cassia.jpg" />
                     <h4 className={styles.pSmall}>Cassia Oliveira <span>&nbsp;&nbsp; 1st Birthray</span></h4>
                 </Plx>
 
@@ -584,7 +593,7 @@ const TwentyEighteen = () => {
                         <div className={styles.pLarge}>Aug</div>
                         <div className={styles.pLarge}>07</div>
                     </div>
-                    <div className={styles.dot}></div>
+                    <Plx tagName="img" parallaxData={ designerHeadshot } className={styles.designerHeadshot} src="images/headshots/pentz-patrick.jpg" />
                     <h4 className={styles.pSmall}>Patrick Pentz <span>&nbsp;&nbsp; 1st Birthray</span></h4>
                 </Plx>
 
@@ -593,7 +602,7 @@ const TwentyEighteen = () => {
                         <div className={styles.pLarge}>Sep</div>
                         <div className={styles.f1}>10</div>
                     </div>
-                    <div className={styles.dot}></div>
+                    <Plx tagName="img" parallaxData={ designerHeadshot } className={styles.designerHeadshot} src="images/headshots/lyons-james.jpg" />
                     <h4 className={styles.pLarge}>James Lyons <span>&nbsp;&nbsp; North America</span></h4>
                 </Plx>
 
@@ -602,7 +611,7 @@ const TwentyEighteen = () => {
                         <div className={styles.pLarge}>Sep</div>
                         <div className={styles.pLarge}>11</div>
                     </div>
-                    <div className={styles.dot}></div>
+                    <Plx tagName="img" parallaxData={ designerHeadshot } className={styles.designerHeadshot} src="images/headshots/-upside-down-face.png" />
                     <h4 className={styles.pSmall}>Gabryelle Barbosa <span>&nbsp;&nbsp; 1st Birthray</span></h4>
                 </Plx>
 
@@ -611,7 +620,7 @@ const TwentyEighteen = () => {
                         <div className={styles.pLarge}>Sep</div>
                         <div className={styles.f1}>17</div>
                     </div>
-                    <div className={styles.dot}></div>
+                    <Plx tagName="img" parallaxData={ designerHeadshot } className={styles.designerHeadshot} src="images/headshots/kim-david.jpg" />
                     <h4 className={styles.pLarge}>David Kim <span>&nbsp;&nbsp; North America</span></h4>
                 </Plx>
 
@@ -620,7 +629,7 @@ const TwentyEighteen = () => {
                         <div className={styles.pLarge}>Sep</div>
                         <div className={styles.pLarge}>18</div>
                     </div>
-                    <div className={styles.dot}></div>
+                    <Plx tagName="img" parallaxData={ designerHeadshot } className={styles.designerHeadshot} src="images/headshots/shama-davi.jpg" />
                     <h4 className={styles.pSmall}>Davi Shamá <span>&nbsp;&nbsp; 1st Birthray</span></h4>
                 </Plx>
 
@@ -629,7 +638,7 @@ const TwentyEighteen = () => {
                         <div className={styles.pLarge}>Oct</div>
                         <div className={styles.f1}>15</div>
                     </div>
-                    <div className={styles.dot}></div>
+                    <Plx tagName="img" parallaxData={ designerHeadshot } className={styles.designerHeadshot} src="images/headshots/manso-miriam.jpg" />
                     <h4 className={styles.pLarge}>Miriam Manso <span>&nbsp;&nbsp; Spain</span></h4>
                 </Plx>
 
@@ -638,7 +647,7 @@ const TwentyEighteen = () => {
                         <div className={styles.pLarge}>Oct</div>
                         <div className={styles.pLarge}>16</div>
                     </div>
-                    <div className={styles.dot}></div>
+                    <Plx tagName="img" parallaxData={ designerHeadshot } className={styles.designerHeadshot} src="images/headshots/jeong-chris.jpg" />
                     <h4 className={styles.pSmall}>Christopher Jeong <span>&nbsp;&nbsp; 1st Birthray</span></h4>
                 </Plx>
 
@@ -647,7 +656,7 @@ const TwentyEighteen = () => {
                         <div className={styles.pLarge}>Oct</div>
                         <div className={styles.pLarge}>18</div>
                     </div>
-                    <div className={styles.dot}></div>
+                    <Plx tagName="img" parallaxData={ designerHeadshot } className={styles.designerHeadshot} src="images/headshots/vazquez-susana.jpg" />
                     <h4 className={styles.pSmall}>Susana Vázquez <span>&nbsp;&nbsp; 1st Birthray</span></h4>
                 </Plx>
 
@@ -656,7 +665,7 @@ const TwentyEighteen = () => {
                         <div className={styles.pLarge}>Dec</div>
                         <div className={styles.pLarge}>18</div>
                     </div>
-                    <div className={styles.dot}></div>
+                    <Plx tagName="img" parallaxData={ designerHeadshot } className={styles.designerHeadshot} src="images/headshots/hancock-abel.jpg" />
                     <h4 className={styles.pSmall}>Abel Hancock <span>&nbsp;&nbsp; 1st Birthray</span></h4>
                 </Plx>
 
@@ -886,6 +895,42 @@ const TwentyEighteen = () => {
                     <div className={styles.p}>Sticky Notes Used</div>
                 </Flex>
             </Plx>
+        </section>
+        
+        {/* lexicon */}
+        <section id="lexicon" className={styles.lexicon}>
+            <div className={styles.navDark}></div>
+            <Plx parallaxData={ PlxUp3 } className={styles.heading}>
+                <h2 className={styles.textHuge}>Lexicon</h2>
+            </Plx>
+            <p className={styles.p}>Nunc in velit venenatis, tempor enim id, sodales eros. Integer rutrum ipsum ac dolor tempus, in lobortis lorem imperdiet. Ut malesuada lectus ac aliquet suscipit. Nullam rhoncus, diam vitae maximus finibus, orci leo dictu.</p>  
+            <div className={styles.wrapper}>
+                <div className={styles.lexiconBackground}>
+                    <div className={styles.justblue}></div>
+                    <div className={styles.svgpattern}></div>
+                </div>
+                <Plx parallaxData={ PlxUp3 } className={styles.grid}>
+                    <div className={styles.image}></div>
+                    <Flex justify="center" align="center" direction="column" className={styles.projects}>
+                        <div className={styles.textLarge}>06</div>
+                        <div className={styles.p}>Projects Using Lexicon</div>
+                    </Flex>
+                    <div className={styles.snapshot}></div>
+                </Plx>
+            </div>
+        </section>
+
+        {/* thank you */}
+        <section id="thankyou" className={styles.thankyou}>
+            <h2 className={styles.f1}>Thank you for a great year</h2>
+            <p className={styles.pLarge}>From the Director</p>
+            <p className={styles.p}>Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Vivamus suscipit tortor eget felis porttitor volutpat. Sed porttitor lectus nibh. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula.
+
+            Nulla porttitor accumsan tincidunt. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Donec rutrum congue leo eget malesuada. Sed porttitor lectus nibh.</p>
+            <Plx parallaxData={ PlxUp2 } className={styles.p}>Juan Hidalgo
+            <Plx parallaxData={ PlxUp1} tagName="img" src="images/2018/jh-signature-hwite.png"/>
+            </Plx>
+
         </section>
 
         <Footer />
