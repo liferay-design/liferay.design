1873a98e4a1bc48597b47833226687c00e30e06a
James Lyons <jamesjlyons29@gmail.com>
2019-02-07 14:54:42 -0800

    add principles text around footer logo

--- src/pages/principles/components/PrinciplesFooter/index.js
+++ src/pages/principles/components/PrinciplesFooter/index.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react'
 import styles from './styles.module.scss'
+import { Link } from 'gatsby'
 
 const PrinciplesFooter = () => {
 	return (
 		<div className={styles.footer}>
 			<div className={styles.footerBadgeOuterCircle}>
-				<div className={styles.footerBadgeCircle}>
-					<img src="/images/principles/principles-badge-words.svg" className={styles.footerBadge} />
-				</div>
+				<Link to="/">
+					<div className={styles.footerBadgeCircle}>
+						<img
+							src="/images/principles/principles-waffle-badge.svg"
+							className={styles.footerBadge}
+						/>
+					</div>
+				</Link>
 			</div>
-			<div className={styles.footerLowerCircle}></div>
+			<div className={styles.footerLowerCircle} />
 		</div>
 	)
 }
