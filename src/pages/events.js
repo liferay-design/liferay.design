910ab1a9b8edbfade40510b771f72a6b222506be
plhnk <paul.hanaoka@gmail.com>
2019-02-26 19:42:35 -0800

    add events page and templates

--- src/pages/events.js
+++ src/pages/events.js
@@ -1,23 +1,50 @@
 import React from 'react'
+import { Link, graphql } from 'gatsby'
 import { MainLayout } from 'components/templates'
-import { Agenda, HeroBanner, Team, Venue } from 'components/organisms'
+import { Posts } from 'components/organisms'
+import { Container, Heading } from 'components/atoms'
 
-const Events = () => (
-	<MainLayout section="Events">
-		<HeroBanner
-			label="event"
-			primaryHeader="Design Week"
-			secondaryHeader="Los Angeles, CA"
-			date="Jan 22 — 26"
-			tertiaryHeader="2018"
-		/>
+export default ({ data }) => {
+	return (
+		<MainLayout section="Events">
+			<Container>
+				<Heading level={1} color="white" padding="4rem">
+					Upcoming Events
+				</Heading>
+				{/* <h4>{data.allMdx.totalCount} Posts</h4> */}
+			<Heading level={1} color="white" padding="4rem">
+				Past Events
+			</Heading>
+				{data.allMdx.edges.map(({ node }) => (
+					<div key={node.id}>
+						<Link to={node.fields.slug}>
+							<h3>{node.frontmatter.title}</h3>
 
-		<Venue />
+							<p>{node.excerpt}</p>
+						</Link>
+					</div>
+				))}
+			</Container>
+		</MainLayout>
+	)
+}
 
-		<Agenda />
-
-		<Team />
-	</MainLayout>
-)
-
-export default Events
+export const query = graphql`
+	{
+		allMdx(filter: { fileAbsolutePath: { regex: "/(events)/(past)/" } }) {
+			totalCount
+			edges {
+				node {
+					id
+					frontmatter {
+						title
+					}
+					fields {
+						slug
+					}
+					excerpt
+				}
+			}
+		}
+	}
+`
