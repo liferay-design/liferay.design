2a22bb6952ffb1a324061412ce094fa312b70d13
plhnk <paul.hanaoka@gmail.com>
2019-03-20 06:57:13 -0700

    structure figma resources grid, clean page

--- src/components/templates/Resources/index.js
+++ src/components/templates/Resources/index.js
@@ -5,17 +5,10 @@ import { Footer, Navbar } from 'components/organisms'
 import { Flex } from 'components/atoms'
 import { withPrefix } from 'gatsby'
 import MDXRenderer from 'gatsby-mdx/mdx-renderer'
-import moment from 'moment'
 
 export default class Resources extends Component {
 	render() {
 		const post = this.props.data.mdx
-
-		// Prereqs for conditional date rendering
-		const d1 = new Date(); // Get today's date
-		const today = d1.getTime(); // convert to date object
-		const d2 = new Date(moment(post.frontmatter.startDateTime).format("YYYY/MM/DD")); // get the date from the frontmatter
-		const eventDate = d2.getTime(); // convert the event's date to a date object
 
 		return (
 			<div>
@@ -24,39 +17,10 @@ export default class Resources extends Component {
 				<Flex justify="center" align="center" className={styles.banner}>
 					<Flex direction="column" className={styles.bannerContent}>
 						<h1>
-							{/* <Flex justify="center" align="center" direction="column">
-								<span>
-									{' '}
-										{moment(post.frontmatter.startDateTime).format("MMM")}
-								</span>
-								<span>
-									{' '}
-										{moment(post.frontmatter.startDateTime).format("DD")}
-								</span>
-							</Flex> */}
 							{post.frontmatter.title}{' '}
 						</h1>
-						{/* <h2>
-							<span className={styles.startTime}>
-								{moment(post.frontmatter.startDateTime).format("h:mma")}
-							</span>
-							{post.frontmatter.endDateTime && <span className={styles.endTime}>
-								&nbsp; – &nbsp;{moment(post.frontmatter.endDateTime).format("h:mma")}
-							</span>}
-							<span className={styles.locationName}>
-								<a href={post.frontmatter.locationURL} target="_blank">
-									{post.frontmatter.locationName}
-								</a>
-							</span>
-						</h2> */}
-						{/* {post.frontmatter.heroImage && <img className={styles.heroImage} src={post.frontmatter.heroImage}/>}
-						<p className={styles.summary}>
-							{post.frontmatter.summary}
-						</p>
-						{post.frontmatter.bodyImage && <img className={styles.bodyImage} src={post.frontmatter.bodyImage}/>} */}
 					</Flex>
 				</Flex>
-				{/* {today < eventDate ? <a href={post.frontmatter.ctaURL} className={styles.ctaButton} target="_blank">RSVP</a> : null } */}
 				<div className={styles.markdownContainer}>
 					<Flex direction="column" className={styles.wrapper}>
 						<MDXRenderer>{post.code.body}</MDXRenderer>
