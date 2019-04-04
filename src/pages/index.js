0bd7ed0394270577ecd84cdd60edfce82eac7f21
plhnk <paul.hanaoka@gmail.com>
2019-04-04 08:53:13 -0700

    add latest posts to homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,55 @@
 import React from 'react'
 import { MainLayout } from 'components/templates'
 import { HeroBanner, Posts } from 'components/organisms'
-import { Container } from 'components/atoms'
+import { CardDefault } from 'components/molecules'
+import { CardGrid, Container, Heading, Link } from 'components/atoms'
 
-const Home = () => (
+export default ({ data }) => {
+	return (
 	<MainLayout>
 		<HeroBanner />
-		{/* <Container>
-			<Posts />
-		</Container> */}
+		<Container>
+			<Heading level={2} color="white" padding="4rem">
+				What's New
+			</Heading>
+			<CardGrid>
+				{data.allMdx.edges.map(({ node }) => (
+					<CardDefault
+						avatarImage
+						key={node.id}
+						imageURL={node.frontmatter.featuredImage}
+						link={node.fields.slug}
+						title={node.frontmatter.title}
+						subtitle={node.frontmatter.description}
+						avatarImageURL={node.frontmatter.avatar}
+					/>
+				))}
+			</CardGrid>
+			{/* <Link to="/articles">More Articles</Link> */}
+		</Container> 
 	</MainLayout>
-)
+)}
 
-export default Home
+export const query = graphql`
+{
+	allMdx(limit: 3, filter: { fileAbsolutePath: { regex: "/(articles)/" } }, sort: { order: DESC, fields: [frontmatter___date] }) {
+		totalCount
+		edges {
+			node {
+				id
+				frontmatter {
+					title
+					description
+					featuredImage
+					author
+					avatar
+					date(formatString: "MMMM DD, YYYY")
+				}
+				fields {
+					slug
+				}
+				excerpt
+			}
+		}
+	}
+}`
