bd5cf838f81eceeff83a81927abf14d01a2111b0
plhnk <paul.hanaoka@gmail.com>
2019-04-04 08:02:52 -0700

    remove featured post from HeroBanner, add heroText

--- src/components/organisms/HeroBanner/index.js
+++ src/components/organisms/HeroBanner/index.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import styles from './styles.module.scss'
-import { FeaturedPost } from 'components/molecules'
+import { Flex, Text } from 'components/atoms'
 
 const HeroBanner = () => (
-	<div className={styles.container}>
-		<FeaturedPost />
-	</div>
+	<Flex className={styles.container} align="center" justify="center">
+		<Text width="20ch" weight="heavy" type="h1" size="xxLarge" color="white">
+			We are{' '}
+			<Text type="span" weight="black" size="xxLarge" color="neutral1">
+				Liferayʼs global team of{' '}
+			</Text>
+			designers.
+		</Text>
+	</Flex>
 )
 
 export default HeroBanner
