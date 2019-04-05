064e48d81c1222a90c04840467860d2d341bd69a
plhnk <paul.hanaoka@gmail.com>
2019-04-05 10:33:16 -0700

    make BillboardAlt scroll sexily

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,34 +7,47 @@ import { CardGrid, Container, Heading, Link, Flex, Button, Text } from 'componen
 export default ({ data }) => {
 	return (
 		<div>
-			<BillboardAlt image="/images/posts/design-week-n.jpg"
-			heading="Join Us!"
-			subtitle="We're hiring Graphic Designers and Product Designers in Frankfurt, Milan, and Los Angeles — check out our careers page to read about the openings!"
-			cta="See if we're a fit"
-			ctaUrl="careers"
-			background="white" />
-			<MainLayout>
-				<HeroBanner />
-				<Container>
-					<Heading level={2} color="white" padding="4rem">
-						What's New
-					</Heading>
-					<CardGrid>
-						{data.allMdx.edges.map(({ node }) => (
-							<CardDefault
-								avatarImage
-								key={node.id}
-								imageURL={node.frontmatter.featuredImage}
-								link={node.fields.slug}
-								title={node.frontmatter.title}
-								subtitle={node.frontmatter.description}
-								avatarImageURL={node.frontmatter.avatar}
-							/>
-						))}
-					</CardGrid>
-					{/* <Link to="/articles">More Articles</Link> */}
-				</Container>
-			</MainLayout>
+			<BillboardAlt
+				image="/images/posts/design-week-n.jpg"
+				heading="Join Us!"
+				subtitle="We're hiring Graphic Designers and Product Designers in Frankfurt, Milan, and Los Angeles — check out our careers page to read about the openings!"
+				cta="See if we're a fit"
+				ctaUrl="careers"
+				background="white"
+			/>
+			<div
+				style={{
+					backgroundAttachment:'fixed',
+					backgroundColor:'#13141f',
+					backgroundImage:'url("/images/diamond-bar-topography.png")',
+					backgroundRpeat:'noRepeat repeat',
+					backgroundSize:'100%',
+					position: 'relative',
+				}}
+			>
+				<MainLayout>
+					<HeroBanner />
+					<Container>
+						<Heading level={2} color="white" padding="4rem">
+							What's New
+						</Heading>
+						<CardGrid>
+							{data.allMdx.edges.map(({ node }) => (
+								<CardDefault
+									avatarImage
+									key={node.id}
+									imageURL={node.frontmatter.featuredImage}
+									link={node.fields.slug}
+									title={node.frontmatter.title}
+									subtitle={node.frontmatter.description}
+									avatarImageURL={node.frontmatter.avatar}
+								/>
+							))}
+						</CardGrid>
+						{/* <Link to="/articles">More Articles</Link> */}
+					</Container>
+				</MainLayout>
+			</div>
 		</div>
 	)}
 
