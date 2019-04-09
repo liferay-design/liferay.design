d22afa305eb6686468df6242e67a57b1fb13ae3b
plhnk <paul.hanaoka@gmail.com>
2019-04-09 06:51:07 -0700

    remove old deploy files and scripts

--- deploy/deploy-dev.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-curl https://cdn.wedeploy.com/cli/latest/wedeploy.sh -fsSL | bash
-
-echo "" > .gitignore
-
-npm run build
-
-echo $AUTH_TOKEN | we login && we deploy -p liferaydotdesign-dev --quiet
