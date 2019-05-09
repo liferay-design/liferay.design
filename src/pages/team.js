8a5ea77b736a5b9a724587eb11453170ec5b1c33
plhnk <paul.hanaoka@gmail.com>
2019-05-08 23:47:15 -0700

    dynamically generate `/team` page based on mdx instead of json

--- src/pages/team.js
+++ src/pages/team.js
@@ -1,17 +1,58 @@
-import { Container, Heading } from 'components/atoms'
-import { Team } from 'components/organisms'
+import { CardGrid, Container, Heading } from 'components/atoms'
+import { CardDefault } from 'components/molecules'
 import { MainLayout } from 'components/templates'
+import { graphql } from 'gatsby'
 import React from 'react'
 
-const TeamPage = () => (
-	<MainLayout section="Team">
-		<Container>
-			<Heading level={1} color="white" padding="4rem">
-				The Squad
-			</Heading>
-			<Team />
-		</Container>
-	</MainLayout>
-)
+export default ({ data }) => {
+	return (
+		<MainLayout section="Team">
+			<Container>
+				<Heading level={1} color="white" padding="4rem">
+					The Squad
+				</Heading>
+				<CardGrid>
+					{data.allMdx.edges.map(({ node }) => (
+						<CardDefault
+							avatarImage
+							key={node.id}
+							imageURL={node.frontmatter.featuredImage}
+							link={node.fields.slug}
+							title={node.frontmatter.title}
+							subtitle={node.frontmatter.description}
+							avatarImageURL={node.frontmatter.avatar}
+						/>
+					))}
+				</CardGrid>
+			</Container>
+		</MainLayout>
+	)
+}
 
-export default TeamPage
+export const query = graphql`
+	{
+		allMdx(
+			filter: { fileAbsolutePath: { regex: "/(/team/)/" } }
+			sort: { order: ASC, fields: [fields___slug] }
+		) {
+			totalCount
+			edges {
+				node {
+					id
+					frontmatter {
+						title
+						description
+						featuredImage
+						author
+						avatar
+						date(formatString: "MMMM DD, YYYY")
+					}
+					fields {
+						slug
+					}
+					excerpt
+				}
+			}
+		}
+	}
+`
