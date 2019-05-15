9b3a608c74268538489b45cee7a2af89535e92e2
plhnk <paul.hanaoka@gmail.com>
2019-05-15 07:16:04 -0700

    add Google Tag Manager

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,6 +20,21 @@ module.exports = {
 				respectDNT: true,
 			},
 		},
+		{
+			resolve: `gatsby-plugin-google-tagmanager`,
+			options: {
+				id: "GTM-WWQK8ZR",
+
+				// Include GTM in development.
+				// Defaults to false meaning GTM will only be loaded in production.
+				includeInDevelopment: false,
+
+				// Specify optional GTM environment details.
+				// gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIROMENT_AUTH_STRING",
+				// gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIROMENT_PREVIEW_NAME",
+				// dataLayerName: "YOUR_DATA_LAYER_NAME",
+			},
+		},
 		'gatsby-plugin-react-helmet',
 		'gatsby-plugin-resolve-src',
 		{
