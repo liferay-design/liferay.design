2b00944e54cb8791f74aa6080ea3e812851d924b
plhnk <paul.hanaoka@gmail.com>
2019-05-29 09:44:22 -0700

    add background and padding functionality to Container component

--- src/components/atoms/Container/index.js
+++ src/components/atoms/Container/index.js
@@ -2,8 +2,15 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import styles from './styles.module.scss'
 
-const Container = ({ children }) => {
-	return <div className={styles.container}>{children}</div>
+const Container = ({ children, background, padding }) => {
+	return (
+		<div
+			className={styles.wrapper}
+			style={{ background: `${background}`, padding: `${padding}` }}
+		>
+			<div className={styles.container}>{children}</div>
+		</div>
+	)
 }
 
 Container.propTypes = {
