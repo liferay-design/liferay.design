4741b97deb04d07867ab29531131fdf8d10c4178
plhnk <paul.hanaoka@gmail.com>
2019-08-21 09:51:20 -0700

    remove old favicon

--- src/html.js
+++ src/html.js
@@ -14,7 +14,6 @@ export default class HTML extends React.Component {
 				{...this.props.htmlAttributes}
 			>
 				<head>
-					<link rel="icon" href="/images/favicon.ico?v=1.0" />
 					<meta charSet="utf-8" />
 					<meta httpEquiv="x-ua-compatible" content="ie=edge" />
 					<GoogleFont typography={typography} />
