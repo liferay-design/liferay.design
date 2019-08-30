61a88bab4687560a71ed64ab66a9a5df00908c27
plhnk <paul.hanaoka@gmail.com>
2019-08-29 23:24:59 -0700

    add boxShadow prop

--- src/components/atoms/Image/index.js
+++ src/components/atoms/Image/index.js
@@ -1,15 +1,29 @@
 import { withPrefix } from 'gatsby'
 import PropTypes from 'prop-types'
 import React from 'react'
+import { elevations } from 'theme'
 
-export const Image = ({ circle, src, alt, ...props }) => {
-	return <img {...props} style={{ borderRadius:`${circle ? "50%" : null }` }} alt={alt} src={`${withPrefix(`${src}`)}`} />
+export const Image = ({ circle, dropShadow, src, alt, ...props }) => {
+	return (
+		<img
+			{...props}
+			style={{
+				borderRadius: `${circle ? '50%' : null}`,
+				boxShadow: `${
+					dropShadow ? '0 0.5rem 8rem -0.5rem rgba(48, 49, 63, 0.16)' : null
+				}`,
+			}}
+			alt={alt}
+			src={`${withPrefix(`${src}`)}`}
+		/>
+	)
 }
 
 Image.propTypes = {
 	src: PropTypes.string,
 	alt: PropTypes.string,
 	circle: PropTypes.string,
+	dropShadow: PropTypes.string,
 }
 
 export default Image
