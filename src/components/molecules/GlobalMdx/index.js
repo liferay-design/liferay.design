9d5c4a0083da4db8252c4511b30e853ead9ac8ff
plhnk <paul.hanaoka@gmail.com>
2019-08-30 15:42:26 -0700

    add DoDont to global wrapper

--- src/components/molecules/GlobalMdx/index.js
+++ src/components/molecules/GlobalMdx/index.js
@@ -2,8 +2,9 @@ import React from 'react'
 import MDXRenderer from 'gatsby-mdx/mdx-renderer'
 import { MDXProvider } from '@mdx-js/react'
 import { Image } from 'components/atoms'
+import { DoDont } from 'components/molecules'
 
-const shortcodes = { Image }
+const shortcodes = { Image, DoDont }
 
 export default ({ children }) => (
 	<MDXProvider components={shortcodes}>
