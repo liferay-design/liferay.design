3c8b1a5b204a7697497b9fc7769dd6bc2024751a
plhnk <paul.hanaoka@gmail.com>
2019-08-30 09:46:30 -0700

    add size options, modify alignment implementation

--- src/components/atoms/Image/index.js
+++ src/components/atoms/Image/index.js
@@ -1,24 +1,53 @@
 import { withPrefix } from 'gatsby'
 import PropTypes from 'prop-types'
 import React from 'react'
-import {colors} from 'theme'
+import {colors, fontSizes } from 'theme'
 
-export const Image = ({ align, caption, circle, dropShadow, rounded, src, alt, ...props }) => {
-	const styles = {
-		float: align,
+export const Image = ({ align, caption, circle, dropShadow, rounded, margin, size, src, alt, ...props }) => {
+
+	const sizes = {
+		small: '50%',
+		medium: '100%',
+		large: '120%',
+		xLarge: '160%',
 	}
+
+	const alignments = {
+		left: {
+			float: 'left',
+			marginLeft: '-8%',
+		},
+		right: {
+			float: 'right',
+			marginRight: '-8%',
+		},
+	}
+	
+	const figureStyles = {
+		...(align ? alignments[align] : { marginLeft:'auto', marginRight:'auto' }), // center images by default
+		...(size ? { width: sizes[size] } : {} ),
+	}
+
+	const imgStyles = {
+		...(circle ? { borderRadius: '50%' } : {}),
+		...(rounded ? { borderRadius: `${fontSizes.micro}` } : {}),
+		...(dropShadow
+			? { boxShadow: '0 0.5rem 8rem -0.5rem rgba(48, 49, 63, 0.16)' }
+			: {}),
+	}
+	
 	return (
-		<figure>
+		<figure
+			style={{
+				margin: `${fontSizes.xLarge}`,
+				...figureStyles,
+			}}
+		>
 			<img
 				{...props}
 				style={{
-					borderRadius: `${circle ? '50%' : null}`,
-					borderRadius: `${rounded ? '.5rem' : null}`,
-					boxShadow: `${
-						dropShadow ? '0 0.5rem 8rem -0.5rem rgba(48, 49, 63, 0.16)' : null
-					}`,
-					display:'block',
-					styles
+					...imgStyles,
+					display: 'block',
 				}}
 				alt={alt}
 				src={`${withPrefix(`${src}`)}`}
@@ -29,19 +58,23 @@ export const Image = ({ align, caption, circle, dropShadow, rounded, src, alt, .
 					fontStyle: 'italic',
 					textAlign: 'center',
 				}}
-			>{caption}</figcaption>
+			>
+				{caption}
+			</figcaption>
 		</figure>
 	)
 }
 
 Image.propTypes = {
 	align: PropTypes.string,
-	src: PropTypes.string,
 	alt: PropTypes.string.isRequired,
 	caption: PropTypes.string,
 	circle: PropTypes.boolean,
 	dropShadow: PropTypes.boolean,
+	margin: PropTypes.string,
 	rounded: PropTypes.boolean,
+	size: PropTypes.string,
+	src: PropTypes.string,
 }
 
 export default Image
