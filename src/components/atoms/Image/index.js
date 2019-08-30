fc6916d028bca8fd2e946310ce99623344e17075
plhnk <paul.hanaoka@gmail.com>
2019-08-29 22:58:34 -0700

    add `circle` prop to Image for round images

--- src/components/atoms/Image/index.js
+++ src/components/atoms/Image/index.js
@@ -2,13 +2,14 @@ import { withPrefix } from 'gatsby'
 import PropTypes from 'prop-types'
 import React from 'react'
 
-export const Image = ({ src, alt, ...props }) => {
-	return <img {...props} alt={alt} src={`${withPrefix(`${src}`)}`} />
+export const Image = ({ circle, src, alt, ...props }) => {
+	return <img {...props} style={{ borderRadius:`${circle ? "50%" : null }` }} alt={alt} src={`${withPrefix(`${src}`)}`} />
 }
 
 Image.propTypes = {
 	src: PropTypes.string,
 	alt: PropTypes.string,
+	circle: PropTypes.string,
 }
 
 export default Image
