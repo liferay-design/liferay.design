c9a0d9f5d88ac5118149463f9f3c8c329e2b22cb
plhnk <paul.hanaoka@gmail.com>
2019-09-05 19:27:37 -0700

    remove unused query

--- src/components/organisms/TopBlogPosts/index.js
+++ src/components/organisms/TopBlogPosts/index.js
@@ -27,7 +27,6 @@ export default () => {
 						fields {
 							slug
 						}
-						excerpt
 					}
 				}
 			}
--- src/pages/events/components/UpcomingEvents/index.js
+++ src/pages/events/components/UpcomingEvents/index.js
@@ -31,7 +31,6 @@ export default () => {
 						fields {
 							slug
 						}
-						excerpt
 					}
 				}
 			}
