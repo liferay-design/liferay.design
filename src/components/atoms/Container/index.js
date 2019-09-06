42627f95af9dd07ada9a2bedccceeafad2c75a4d
plhnk <paul.hanaoka@gmail.com>
2019-09-06 12:38:16 -0700

    add Heading to Container, update homepage

--- src/components/atoms/Container/index.js
+++ src/components/atoms/Container/index.js
@@ -1,20 +1,35 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 import styles from './styles.module.scss'
+import { Heading, Flex, Link } from 'components/atoms'
+import { spacing } from 'theme'
 
-const Container = ({ children, background, padding }) => {
+const Container = ({ children, background, padding, linkDestination, linkText, heading, ...props }) => {
 	return (
 		<div
 			className={styles.wrapper}
 			style={{ background: `${background}`, padding: `${padding}` }}
 		>
-			<div className={styles.container}>{children}</div>
+			<div className={styles.container}>
+				<Flex justify="space-between">
+					<Heading color="white" level={2} padding={spacing.medium}{...props}>
+						{heading}
+					</Heading>
+					<Link to={linkDestination}>{linkText}</Link>
+				</Flex>
+				{children}
+			</div>
 		</div>
 	)
 }
 
 Container.propTypes = {
 	children: PropTypes.node,
+	heading: PropTypes.string,
+	padding: PropTypes.string,
+	background: PropTypes.string,
+	linkDestination: PropTypes.string,
+	linkText: PropTypes.string,
 }
 
 export default Container
