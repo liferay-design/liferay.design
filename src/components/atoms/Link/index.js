cf60d2ebcf83c9d7c50e7a19f7682d27f7b3e95f
plhnk <paul.hanaoka@gmail.com>
2019-09-06 14:44:50 -0700

    clean Link component

--- src/components/atoms/Link/index.js
+++ src/components/atoms/Link/index.js
@@ -1,7 +1,5 @@
-import { Link as GatsbyLink } from 'gatsby'
 import AniLink from 'gatsby-plugin-transition-link/AniLink'
 import React from 'react'
-import {colors} from 'theme'
 
 // Since DOM elements <a> cannot receive activeClassName,
 // destructure the prop here and pass it only to GatsbyLink
@@ -16,8 +14,7 @@ const Link = ({ children, to, activeClassName, ...other }) => {
 		return (
 			<AniLink
 				fade
-				bg="red"
-				duration={.5}
+				duration={.4}
 				to={to}
 				activeClassName={activeClassName}
 				{...other}
