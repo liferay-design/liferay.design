583e28597c66c2cf07ec26403abdbfe9aaab4e16
plhnk <paul.hanaoka@gmail.com>
2019-10-15 13:59:02 -0700

    add left alignment, make button conditional in <Banner>

--- src/components/organisms/Banner/index.js
+++ src/components/organisms/Banner/index.js
@@ -1,26 +1,48 @@
-import { Flex, Heading, Link } from 'components/atoms'
+import { Flex, Heading, Link, Container } from 'components/atoms'
 import Navbar from 'components/organisms/Navbar'
 import PropTypes from 'prop-types'
 import React from 'react'
 import { Button, Grid } from 'reakit'
 import styles from './styles.module.scss'
 
-const Banner = ({ underlineColor, headline, subtitle, cta, ctaLink, section, background }) => (
-	<Grid templateRows="8rem 1fr" className={styles.container} style={{ background: `${background}` }}>
+const Banner = ({
+	underlineColor,
+	headline,
+	subtitle,
+	cta,
+	ctaLink,
+	section,
+	background,
+	textLeft
+}) => (
+	<Grid
+		templateRows="8rem 1fr"
+		className={styles.container}
+		style={{ background: `${background}` }}
+	>
 		<Navbar underlineColor={underlineColor} white section={section} />
-		<Flex direction="column" align="center" style={{alignSelf:"center", marginBottom:"8rem"}} className={styles.content}>
-			<Heading color="white" level={1}>
-				{headline}
-			</Heading>
-			<Heading color="white" level={2}>
-				{subtitle}
-			</Heading>
-			<Link to={ctaLink}>
-				<Button className={styles.button} raised primary>
-					{cta}
-				</Button>
-			</Link>
-		</Flex>
+		<Container style={{alignSelf:'center'}}>
+			<Flex
+				direction="column"
+				align="center"
+				style={textLeft ? {textAlign:'left'} : {textAlign:'center'}}
+				className={styles.content}
+			>
+				<Heading color="white" level={1}>
+					{headline}
+				</Heading>
+				<Heading color="white" level={2}>
+					{subtitle}
+				</Heading>
+				{cta ? (
+					<Link to={ctaLink}>
+						<Button className={styles.button} raised primary>
+							{cta}
+						</Button>
+					</Link>
+				) : null}
+			</Flex>
+		</Container>
 	</Grid>
 )
 
