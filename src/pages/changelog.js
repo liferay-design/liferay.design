a75b11896f79e7760de9ae99be45a8af840443c8
plhnk <paul.hanaoka@gmail.com>
2019-10-16 12:10:57 -0700

    rough out /Changelog page

---
+++ src/pages/changelog.js
@@ -0,0 +1,74 @@
+import { Icon, Image, Container, Heading, Link, Flex, Text } from 'components/atoms'
+import { MainLayout } from 'components/templates'
+import { colors, fontSizes, fontWeights } from 'theme'
+import { graphql } from 'gatsby'
+import React from 'react'
+import { Avatar } from 'react-md'
+
+export default ({ data }) => {
+	return (
+		<MainLayout section="Changelog">
+			<Container background="white">
+				<Flex direction="row" justify="space-between">
+					<Heading level={1} color="white" padding="4rem">
+						The Changelog
+					</Heading>
+				</Flex>
+				<Flex direction="column">
+					{data.allChangelogYaml.edges.map(({ node }) => (
+						<Flex margin="4rem 0" direction="column" key={node.id}>
+							{node.icon ? <Icon name={node.icon} /> : ''}
+							<Link to={node.url}>
+								<Heading level="2">
+									{node.title}
+									<Icon
+										name="externalLink"
+										width=".6em"
+										margin="0 0 0 .5em"
+									/>
+								</Heading>
+							</Link>
+							{node.longSummary ? <Text>{node.longSummary}</Text> : ''}
+							<Text>{node.id}</Text>
+							<Text>{node.url}</Text>
+							Updated by: <Text>{node.author.id}</Text>
+							<Avatar src={node.author.headshot} />
+							{node.contributors ? (
+								<Text>
+									With contributions from: {node.contributors.map(({i}) => (
+										<Text>{i.contributors}</Text>
+									))}
+								</Text>
+							) : (
+								''
+							)}
+						</Flex>
+					))}
+				</Flex>
+			</Container>
+		</MainLayout>
+	)
+}
+
+export const query = graphql`
+			{
+				allChangelogYaml(sort: { order: DESC, fields: [id] }) {
+					totalCount
+					edges {
+						node {
+							id
+							url
+							author {
+								id
+								headshot
+							}
+							title
+							icon
+							longSummary
+							contributors
+							buildPreview
+						}
+					}
+				}
+			}
+		`
