9a42d538805893cb43a13a560b68b84f0e68a498
plhnk <paul.hanaoka@gmail.com>
2019-10-16 13:43:32 -0700

    componentize Changelog stuff

---
+++ src/components/organisms/ChangelogFeed/index.js
@@ -0,0 +1,76 @@
+import React from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
+import { Heading, Text, Flex, Link, Icon } from 'components/atoms'
+import Changelog from 'pages/changelog'
+import PropTypes from 'prop-types'
+import {Avatar} from 'react-md'
+
+export default function ChangelogFeed ({ items, ...props }) {
+	const data = useStaticQuery(graphql`
+		{
+			allChangelogYaml(sort: { order: DESC, fields: [id] }) {
+				totalCount
+				edges {
+					node {
+						id
+						url
+						author {
+							id
+							headshot
+						}
+						title
+						icon
+						longSummary
+						contributors {
+							id
+							headshot
+						}
+						buildPreview
+					}
+				}
+			}
+		}
+	`)
+
+	const Feed = data.allChangelogYaml.edges
+		// .filter(edges => edges.node.frontmatter.author.slug === teammate && edges.node.id !== currentPost)
+		.slice(0, `${items}` )
+		.map(({ node }) => (
+			<Flex margin="4rem 0" direction="column" key={node.id}>
+				{node.icon ? <Icon name={node.icon} /> : ''}
+				<Link to={node.url}>
+					<Heading level="2">
+						{node.title}
+						<Icon name="externalLink" width=".6em" margin="0 0 0 .5em" />
+					</Heading>
+				</Link>
+				{node.longSummary ? <Text>{node.longSummary}</Text> : ''}
+				<Text>{node.id}</Text>
+				<Text>{node.url}</Text>
+				Updated by: <Text>{node.author.id}</Text>
+				<Avatar src={node.author.headshot} />
+				{node.contributors ? (
+					<Text>
+						With contributions from:{' '}
+						{node.contributors.map(i => (
+							<div>
+								<Avatar title={i.id} src={i.headshot} alt={i.id} />
+							</div>
+						))}
+					</Text>
+				) : (
+					''
+				)}
+			</Flex>
+		))
+
+	return <div>{Feed}</div>
+}
+
+Changelog.defaultProps = {
+	items: 'Infinity',
+}
+
+ChangelogFeed.propTypes = {
+	items: PropTypes.string,
+}
--- src/pages/changelog.js
+++ src/pages/changelog.js
@@ -1,79 +1,22 @@
-import { Icon, Image, Container, Heading, Link, Flex, Text } from 'components/atoms'
+import { Container, Heading, Flex } from 'components/atoms'
 import { MainLayout } from 'components/templates'
-import { colors, fontSizes, fontWeights } from 'theme'
-import { graphql } from 'gatsby'
 import React from 'react'
-import { Avatar } from 'react-md'
+import { ChangelogFeed } from 'components/organisms'
 
-export default ({ data }) => {
-	return (
+const Changelog = () => 
+(
 		<MainLayout section="Changelog">
 			<Container background="white">
 				<Flex direction="row" justify="space-between">
-					<Heading level={1} color="white" padding="4rem">
+					<Heading level={1} color="black" padding="4rem">
 						The Changelog
 					</Heading>
 				</Flex>
 				<Flex direction="column">
-					{data.allChangelogYaml.edges.map(({ node }) => (
-						<Flex margin="4rem 0" direction="column" key={node.id}>
-							{node.icon ? <Icon name={node.icon} /> : ''}
-							<Link to={node.url}>
-								<Heading level="2">
-									{node.title}
-									<Icon
-										name="externalLink"
-										width=".6em"
-										margin="0 0 0 .5em"
-									/>
-								</Heading>
-							</Link>
-							{node.longSummary ? <Text>{node.longSummary}</Text> : ''}
-							<Text>{node.id}</Text>
-							<Text>{node.url}</Text>
-							Updated by: <Text>{node.author.id}</Text>
-							<Avatar src={node.author.headshot} />
-							{node.contributors ? (
-								<Text>
-									With contributions from: {node.contributors.map((i) => (
-										<div>
-											<Avatar title={i.id} src={i.headshot} alt={i.id} />
-										</div>
-									))}
-								</Text>
-							) : (
-								''
-							)}
-						</Flex>
-					))}
+					<ChangelogFeed items="4"/>
 				</Flex>
 			</Container>
 		</MainLayout>
 	)
-}
 
-export const query = graphql`
-			{
-				allChangelogYaml(sort: { order: DESC, fields: [id] }) {
-					totalCount
-					edges {
-						node {
-							id
-							url
-							author {
-								id
-								headshot
-							}
-							title
-							icon
-							longSummary
-							contributors {
-								id
-								headshot
-							}
-							buildPreview
-						}
-					}
-				}
-			}
-		`
+export default Changelog
