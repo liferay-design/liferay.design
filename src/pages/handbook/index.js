2d6d38a3cfc35b06de79a88210aebe26ebc64daf
plhnk <paul.hanaoka@gmail.com>
2019-10-17 09:42:47 -0700

    make handbook page ultra mobile friendly

--- src/pages/handbook/index.js
+++ src/pages/handbook/index.js
@@ -11,6 +11,14 @@ import { colors, spacing } from 'theme'
 
 const Handbook = () => {
 	const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1100px)' })
+	const gridTemplate = `	"a a . . . ." 
+                                "a a b b . ." 
+                                "a a b b c c" 
+                                ". . b b c c" 
+                                ". . . . c c" auto / 1fr 1fr 1fr 1fr 1fr 1fr`
+	const mobileGridTemplate = `"a" 
+                                "b" 
+                                "c" auto / 1fr `
 	return (
 		<div
 			className={`${blueprints.theme} ${documentation.theme} ${
@@ -28,73 +36,52 @@ const Handbook = () => {
 			/>
 			<div className={styles.wrapper}>
 				<Container>
-					<MediaQuery maxWidth={767}>
-						{matches => {
-							let gridTemplate = matches
-								? `	"tlg" auto // first is mobile
-								"tlc" auto
-								"m" auto
-								"blc" auto
-								"brc" auto / auto-fit, minmax(200px, 1fr)`
-								: `	"a a . . . ." 
-                                "a a b b . ." 
-                                "a a b b c c" 
-                                ". . b b c c" 
-                                ". . . . c c" auto / 1fr 1fr 1fr 1fr 1fr 1fr`
-                                // "d d d e e e" 
-							return (
-								<Grid
-									className={styles.teasers}
-									gap="2rem"
-									template={gridTemplate}
-								>
-									<Grid.Item
-										area="a"
-										marginTop="-8rem"
-									>
-										<CardLexicon
-											handbook
-											preTitle="First Things First"
-											title="Principles"
-											text="These principles guide our work, and provide standards for what good design means at Liferay."
-											to="/handbook/welcome"
-										/>
-									</Grid.Item>
-									<Grid.Item
-										area="b"
-										marginTop="-8rem"
-									>
-										<CardLexicon
-											delay="0.1s"
-											handbook
-											preTitle="Get Things Done"
-											title="Work"
-											text="Policies, guidelines, and best-practices for creating value in Liferay’s Design Department."
-											to="/handbook/work/remotely"
-										/>
-									</Grid.Item>
-									<Grid.Item
-										area="c"
-										marginTop="-8rem"
-									>
-										<CardLexicon
-											delay="0.2s"
-											handbook
-											preTitle="Better Every Day"
-											title="Grow"
-											text="A core trait of a Liferay Designer is a desire for growth — see what that looks like and how we do it."
-											to="/handbook/grow/introduction"
-										/>
-									</Grid.Item>
-								</Grid>
-							)
-						}}
-					</MediaQuery>
-					<div style={{marginTop:spacing.xxLarge}}>
+					<Grid
+						className={styles.teasers}
+						gap="2rem"
+						template={isTabletOrMobile ? mobileGridTemplate : gridTemplate}
+					>
+						<Grid.Item
+							area="a"
+						>
+							<CardLexicon
+								handbook
+								preTitle="First Things First"
+								title="Principles"
+								text="These principles guide our work, and provide standards for what good design means at Liferay."
+								to="/handbook/welcome"
+							/>
+						</Grid.Item>
+						<Grid.Item
+							area="b"
+						>
+							<CardLexicon
+								delay="0.1s"
+								handbook
+								preTitle="Get Things Done"
+								title="Work"
+								text="Policies, guidelines, and best-practices for creating value in Liferay’s Design Department."
+								to="/handbook/work/remotely"
+							/>
+						</Grid.Item>
+						<Grid.Item
+							area="c"
+						>
+							<CardLexicon
+								delay="0.2s"
+								handbook
+								preTitle="Better Every Day"
+								title="Grow"
+								text="A core trait of a Liferay Designer is a desire for growth — see what that looks like and how we do it."
+								to="/handbook/grow/introduction"
+							/>
+						</Grid.Item>
+					</Grid>
+					<div className={styles.gagb}>
 						<Text color="lightGrey" weight="inherit" className={styles.date}>
 							Since 2004
 						</Text>
-						<Heading className={styles.gagb}>
+						<Heading>
 							Growing &amp; <br />
 							Getting Better. <br />
 							<span>Together.</span>
