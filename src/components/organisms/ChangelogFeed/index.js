b42d3ffff3551252ca0a79234e13f08482c48a76
plhnk <paul.hanaoka@gmail.com>
2019-10-19 02:10:45 -0700

    roughly style /Changelog

--- src/components/organisms/ChangelogFeed/index.js
+++ src/components/organisms/ChangelogFeed/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useStaticQuery, graphql } from 'gatsby'
-import { Heading, Text, Flex, Link, Icon } from 'components/atoms'
+import { Heading, Text, Flex, Link, Icon, Image } from 'components/atoms'
 import PropTypes from 'prop-types'
 import { Avatar } from 'react-md'
 import moment from 'moment'
@@ -37,55 +37,61 @@ export default function ChangelogFeed({ items, ...props }) {
 	`)
 
 	const Feed = data.allChangelogYaml.edges.slice(0, `${items}`).map(({ node }) => (
-		<Flex className={styles.wrapper}>
+		<Flex margin="4rem 0" key={node.id} className={styles.wrapper}>
 			<Date
-				size='2rem'
+				size="2rem"
 				color={colors.primaryd2}
 				month={moment(node.id).format('MMM')}
 				day={moment(node.id).format('DD')}
 			/>
-			<Flex margin="4rem 0" direction="column" key={node.id}>
+			<Flex direction="column" margin="0 1rem" width="100%">
 				<Link to={node.titleUrl}>
-					<Heading level="2">
+					<Heading level={2}>
 						{node.title}{' '}
 						{node.titleUrl ? (
 							<Icon name="externalLink" width=".6em" />
 						) : null}
+						{node.longSummary ? <Text>{node.longSummary}</Text> : ''}
 					</Heading>{' '}
 				</Link>
-				{node.longSummary ? <Text>{node.longSummary}</Text> : ''}
-				<Text>{node.id}</Text>
-				<Link
-					to={
-						'https://github.com/liferay-design/liferay.design' +
-						`${node.gitUrl}`
-					}
-				>
-					<Icon name="github" width="1em" margin="0 0 0 .5em" />
-					<Text>See full details on Github.</Text>
-				</Link>
-				<Text>Author: {node.author.id}</Text>
-				<Flex>
-					<Avatar
-						src={node.author.headshot}
-						title={node.author.id}
-						alt={`${node.author.id}` + '‘s headshot'}
-					/>
-					{node.contributors ? (
-						<>
-							{node.contributors.map(i => (
-								<div>
-									<Avatar
-										title={i.id}
-										src={i.headshot}
-										alt={`${i.id}` + '‘s headshot'}
-									/>
-								</div>
-							))}
-						</>
-					) : (
-						''
-					)}
+				<Flex justify="space-between">
+					<Flex direction='row-reverse' className={styles.avatarWrapper}>
+						{node.contributors ? (
+							<>
+								{node.contributors.map(i => (
+									<div>
+										<Image
+											className={styles.avatar}
+											role='presentation'
+											title={i.id}
+											src={i.headshot}
+											alt={`${i.id}` + '‘s headshot'}
+										/>
+									</div>
+								))}
+							</>
+						) : (
+							''
+						)}
+						<Image
+							className={styles.avatar}
+							role='presentation'
+							src={node.author.headshot}
+							title={node.author.id}
+							alt={`${node.author.id}` + '‘s headshot'}
+						/>
+					</Flex>
+					<Link
+						to={
+							'https://github.com/liferay-design/liferay.design' +
+							`${node.gitUrl}`
+						}
+					>
+						<Flex align="center" className={styles.github}>
+							<Icon name="github" width="1em" margin="0 .5em" />
+							<Text>View on Github</Text>
+						</Flex>
+					</Link>
 				</Flex>
 			</Flex>
 		</Flex>
