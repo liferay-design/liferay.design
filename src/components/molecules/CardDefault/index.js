9a8d560225fb0e8507919840aa8267f6ba1932e5
plhnk <paul.hanaoka@gmail.com>
2019-10-24 23:04:12 -0700

    add AnimateIn to CardDefault

--- src/components/molecules/CardDefault/index.js
+++ src/components/molecules/CardDefault/index.js
@@ -1,4 +1,4 @@
-import { Icon, Text, Link, Image } from 'components/atoms'
+import { Icon, Text, Link, Image, AnimateIn } from 'components/atoms'
 import { withPrefix } from 'gatsby'
 import PropTypes from 'prop-types'
 import React from 'react'
@@ -6,6 +6,7 @@ import { Avatar, Card, CardText, CardTitle, Media } from 'react-md'
 import styles from './styles.module.scss'
 
 const CardDefault = ({
+	delay,
 	imageURL,
 	icon,
 	title,
@@ -17,44 +18,53 @@ const CardDefault = ({
 	tag,
 	...props
 }) => (
-	<Link className={styles.cardLink} to={link}>
-		<Media style={{
-			position: "absolute",
-			top: "0",
-			bottom: "0",
-			right: "0",
-			left: "0",
-			overflow: "visible",
-		}} aspectRatio="4-3" className={styles.sexyShadow} {...props}>
-			<Image alt="shadow for image" src={imageURL} />
-		</Media>
-		<Card className={styles.card}>
-			<Media aspectRatio="4-3" className={styles.null} {...props}>
-				<Image alt={'image for ' + `${title}`} src={imageURL} />
+	<AnimateIn delay={delay}>
+		<Link className={styles.cardLink} to={link}>
+			<Media
+				style={{
+					position: 'absolute',
+					top: '0',
+					bottom: '0',
+					right: '0',
+					left: '0',
+					overflow: 'visible',
+				}}
+				aspectRatio="4-3"
+				className={styles.sexyShadow}
+				{...props}
+			>
+				<Image alt="shadow for image" src={imageURL} />
 			</Media>
-			{pill ? (
-				<Text className={styles.pill} weight="black">
-					{tag}
-				</Text>
-			) : null}
-			{avatarImage ? (
-				<Avatar
-					className={styles.avatar}
-					alt={avatarImageURL}
-					src={withPrefix(`${avatarImageURL}`)}
-				/>
-			) : (
-				<Avatar
-					className={styles.avatar}
-					icon={<Icon fill="white" height="2rem" width="2rem" name={icon} />}
-				/>
-			)}
-			<div className={styles.copy}>
-				<CardTitle className={styles.title} title={title} />
-				<CardText className={styles.text}>{subtitle}</CardText>
-			</div>
-		</Card>
-	</Link>
+			<Card className={styles.card}>
+				<Media aspectRatio="4-3" className={styles.null} {...props}>
+					<Image alt={'image for ' + `${title}`} src={imageURL} />
+				</Media>
+				{pill ? (
+					<Text className={styles.pill} weight="black">
+						{tag}
+					</Text>
+				) : null}
+				{avatarImage ? (
+					<Avatar
+						className={styles.avatar}
+						alt={avatarImageURL}
+						src={withPrefix(`${avatarImageURL}`)}
+					/>
+				) : (
+					<Avatar
+						className={styles.avatar}
+						icon={
+							<Icon fill="white" height="2rem" width="2rem" name={icon} />
+						}
+					/>
+				)}
+				<div className={styles.copy}>
+					<CardTitle className={styles.title} title={title} />
+					<CardText className={styles.text}>{subtitle}</CardText>
+				</div>
+			</Card>
+		</Link>
+	</AnimateIn>
 )
 
 CardDefault.propTypes = {
--- src/components/molecules/CardHidden/index.js
+++ src/components/molecules/CardHidden/index.js
@@ -1,4 +1,4 @@
-import { Flex, Heading, Icon, Link, Text } from 'components/atoms'
+import { Flex, Heading, Icon, Link, Text, AnimateIn } from 'components/atoms'
 import PropTypes from 'prop-types'
 import React from 'react'
 import styles from './styles.module.scss'
@@ -12,37 +12,57 @@ const CardHidden = ({
 	to,
 }) => {
 	return (
-		<Link to={to} className={styles.cardHidden}>
-			<Flex
-				className={styles.card}
-				padding={icon ? "2rem 0" : ".5rem"}
-				style={delay ? { animationDelay: `${delay}` } : null}
-			>
-				{icon ? (
-					<Flex align="flex-start" justify="center" className={styles.icon}>
-						<Icon
-							fill="primary"
+		<AnimateIn delay={delay}>
+			<Link to={to} className={styles.cardHidden}>
+				<Flex
+					className={styles.card}
+					padding={icon ? '2rem 0' : '.5rem'}
+					style={delay ? { animationDelay: `${delay}` } : null}
+				>
+					{icon ? (
+						<Flex
+							align="flex-start"
+							justify="center"
 							className={styles.icon}
-							name={icon}
-							height="4em"
-							width="4em"
-							margin="0 .5rem 0 1.5rem"
-						/>
+						>
+							<Icon
+								fill="primary"
+								className={styles.icon}
+								name={icon}
+								height="4em"
+								width="4em"
+								margin="0 .5rem 0 1.5rem"
+							/>
+						</Flex>
+					) : null}
+					<Flex direction="column" className={styles.copy}>
+						<Heading
+							level={icon ? `${2}` : `${3}`}
+							className={styles.title}
+						>
+							{title}
+						</Heading>
+						<Text
+							type="p"
+							size={icon ? 'medium' : 'base'}
+							className={styles.text}
+						>
+							{text}
+						</Text>
+						{cta ? (
+							<Text
+								type="p"
+								size="base"
+								weight="black"
+								className={styles.cta}
+							>
+								{cta}
+							</Text>
+						) : null}
 					</Flex>
-				) : null}
-				<Flex direction="column" className={styles.copy}>
-					<Heading level={icon ? `${2}` : `${3}`} className={styles.title}>
-						{title}
-					</Heading>
-					<Text type="p" size={icon ? "medium" : "base"} className={styles.text}>
-						{text}
-					</Text>
-					{cta ? <Text type="p" size="base" weight="black" className={styles.cta}>
-						{cta}
-					</Text> : null}
 				</Flex>
-			</Flex>
-		</Link>
+			</Link>
+		</AnimateIn>
 	)
 }
 
