47a8e0fb825efb96edb71bd3cf2b3d2742da1923
plhnk <paul.hanaoka@gmail.com>
2019-10-24 23:00:55 -0700

    create AnimateIn component, apply to CardLexicon

---
+++ src/components/atoms/AnimateIn/index.js
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+import styles from './styles.module.scss'
+
+const AnimateIn = ({
+	delay,
+	children
+}) => {
+
+	return (
+		<div className={styles.animateIn} style={ delay ? {animationDelay:`${delay}`} : null }>
+			{children}
+		</div>
+	)
+}
+
+AnimateIn.propTypes = {
+	delay: PropTypes.string,
+}
+
+export default AnimateIn
