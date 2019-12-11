3d6a38e45a99ad87754b362e2c120912ff86c20f
Abel Hancock <abel.hancock@liferay.com>
2019-12-11 11:00:23 -0800

    add active state for anchor nav items

--- src/pages/2019/components/Nav/index.js
+++ src/pages/2019/components/Nav/index.js
@@ -3,6 +3,168 @@ import AnchorLink from 'react-anchor-link-smooth-scroll'
 import Plx from 'react-plx'
 import styles from '../../styles.module.scss'
 
+const top = [
+	{
+		start: 0,
+		end: '#team',
+		properties: [
+			{
+				startValue: '#006DE7',
+				endValue: '#006DE7',
+				property: 'backgroundColor',
+			},
+			{
+				startValue: 2.5,
+				endValue: 2.5,
+				property: 'scale',
+			},
+		],
+	},
+	{
+		start: '#team',
+		duration: 100,
+		startOffset: '25vh',
+		properties: [
+			{
+				startValue: '#006DE7',
+				endValue: '#6B6C7E',
+				property: 'backgroundColor',
+			},
+			{
+				startValue: 2.5,
+				endValue: 1,
+				property: 'scale',
+			},
+		],
+	},
+]
+const team = [
+	{
+		start: '#team',
+		duration: 100,
+		startOffset: '25vh',
+		properties: [
+			{
+				startValue: '#6b6c7e',
+				endValue: '#006DE7',
+				property: 'backgroundColor',
+			},
+			{
+				startValue: 1,
+				endValue: 2.5,
+				property: 'scale',
+			},
+		],
+	},
+	{
+		start: '#initiatives',
+		duration: 100,
+		startOffset: '25vh',
+		properties: [
+			{
+				startValue: '#006DE7',
+				endValue: '#6B6C7E',
+				property: 'backgroundColor',
+			},
+			{
+				startValue: 2.5,
+				endValue: 1,
+				property: 'scale',
+			},
+		],
+	},
+]
+const initiatives = [
+	{
+		start: '#initiatives',
+		duration: 100,
+		startOffset: '25vh',
+		properties: [
+			{
+				startValue: '#6b6c7e',
+				endValue: '#006DE7',
+				property: 'backgroundColor',
+			},
+			{
+				startValue: 1,
+				endValue: 2.5,
+				property: 'scale',
+			},
+		],
+	},
+	{
+		start: '#projects',
+		duration: 100,
+		startOffset: '25vh',
+		properties: [
+			{
+				startValue: '#006DE7',
+				endValue: '#6B6C7E',
+				property: 'backgroundColor',
+			},
+			{
+				startValue: 2.5,
+				endValue: 1,
+				property: 'scale',
+			},
+		],
+	},
+]
+const projects = [
+	{
+		start: '#projects',
+		duration: 100,
+		startOffset: '25vh',
+		properties: [
+			{
+				startValue: '#6b6c7e',
+				endValue: '#006DE7',
+				property: 'backgroundColor',
+			},
+			{
+				startValue: 1,
+				endValue: 2.5,
+				property: 'scale',
+			},
+		],
+	},
+	{
+		start: '#ops',
+		duration: 100,
+		startOffset: '25vh',
+		properties: [
+			{
+				startValue: '#006DE7',
+				endValue: '#6B6C7E',
+				property: 'backgroundColor',
+			},
+			{
+				startValue: 2.5,
+				endValue: 1,
+				property: 'scale',
+			},
+		],
+	},
+]
+const ops = [
+	{
+		start: '#ops',
+		duration: 100,
+		startOffset: '25vh',
+		properties: [
+			{
+				startValue: '#6b6c7e',
+				endValue: '#006DE7',
+				property: 'backgroundColor',
+			},
+			{
+				startValue: 1,
+				endValue: 2.5,
+				property: 'scale',
+			},
+		],
+	}
+]
 export default class Nav extends Component {
 	constructor(props) {
         super(props);
@@ -40,43 +202,48 @@ export default class Nav extends Component {
 				<div className={styles.linkWrapper} >
 					<AnchorLink href="#top" >
 						<Plx
+							parallaxData={top}
 							className={styles.anchorBlock}
 						/>
-						<Plx className={styles.anchorText}>
+						<div className={styles.anchorText}>
 							Top
-						</Plx>
+						</div>
 					</AnchorLink>
 					<AnchorLink offset="100" href="#team">
 						<Plx
+							parallaxData={team}
 							className={styles.anchorBlock}
 						/>
-						<Plx className={styles.anchorText}>
+						<div className={styles.anchorText}>
 							Team
-						</Plx>
+						</div>
 					</AnchorLink>
 					<AnchorLink href="#initiatives">
 						<Plx
+							parallaxData={initiatives}
 							className={styles.anchorBlock}
 						/>
-						<Plx className={styles.anchorText}>
+						<div className={styles.anchorText}>
 							Initiatives
-						</Plx>
+						</div>
 					</AnchorLink>
 					<AnchorLink offset="200" href="#projects">
 						<Plx
+							parallaxData={projects}
 							className={styles.anchorBlock}
 						/>
-						<Plx className={styles.anchorText}>
+						<div className={styles.anchorText}>
 							Projects
-						</Plx>
+						</div>
 					</AnchorLink>
 					<AnchorLink href="#ops">
 						<Plx
+							parallaxData={ops}
 							className={styles.anchorBlock}
 						/>
-						<Plx className={styles.anchorText}>
+						<div className={styles.anchorText}>
 							Ops
-						</Plx>
+						</div>
 					</AnchorLink>
 				</div>
 			</Plx>
