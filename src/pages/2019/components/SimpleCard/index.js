7e799cbbbb2a13bf17b70c1bc1a06e2c45066f78
Abel Hancock <abel.hancock@liferay.com>
2019-12-17 11:18:24 -0800

    make simple card logo an image instead of background image

--- src/pages/2019/components/SimpleCard/index.js
+++ src/pages/2019/components/SimpleCard/index.js
@@ -1,3 +1,4 @@
+import { withPrefix } from 'gatsby'
 import { Flex, Heading } from 'components/atoms'
 import PropTypes from 'prop-types'
 import React from 'react'
@@ -11,7 +12,7 @@ const SimpleCard = ({ image, title, subtitle, description }) => {
             justify="center"
             align="center"
             className={styles.simpleCard}>
-                <div style={{ backgroundImage: `url( ${image} )` }} className={styles.cardImage} />
+                <img src={`${withPrefix(`${image}`)}`} className={styles.cardImage} />
                 <Heading level="3" className={styles.f3}>{title}</Heading>
                 <span className={styles.pSmall}>{subtitle}</span>
                 <p className={styles.p}>{description}</p>
