9f5b68e06606e2c865b76d1696622e3699678bd5
Abel Hancock <abel.hancock@liferay.com>
2019-12-18 10:26:34 -0800

    remove deleted components from index

--- src/pages/2019/components/_index.js
+++ src/pages/2019/components/_index.js
@@ -1,17 +1,13 @@
-import BrandCarousel from './BrandCarousel'
 import Designer from './Designer'
 import KeyArt from './KeyArt'
 import Nav from './Nav'
 import ProjectsCarousel from './ProjectsCarousel'
 import SimpleCard from './SimpleCard'
-import VanityMetric from './VanityMetric'
 
 export { 
-    BrandCarousel, 
     Designer, 
     KeyArt, 
     Nav, 
     ProjectsCarousel, 
-    SimpleCard, 
-    VanityMetric 
+    SimpleCard
 }
