e3237d179694dccd65195c3956e1a6dc86e8f9c2
Abel Hancock <abel.hancock@liferay.com>
2019-12-19 15:15:53 -0800

    add justify prop to content component

--- src/pages/2019/components/Content/index.js
+++ src/pages/2019/components/Content/index.js
@@ -2,9 +2,9 @@ import { Heading, Text, Flex } from 'components/atoms'
 import React from 'react'
 import styles from './styles.module.scss'
 
-const Content = ({ pretitle, title, copy, align }) => {
+const Content = ({ pretitle, title, copy, align, justify }) => {
 	return (
-		<Flex align={align} direction="column">
+		<Flex align={align} justify={justify} direction="column">
             <Text weight="black" color="primary" className={styles.preHeader}>{pretitle}</Text>
             <Heading className={styles.f2} color="white" level={2}>{title}</Heading>
             <Text align={align} size="medium" className={styles.p}>{copy}</Text>
