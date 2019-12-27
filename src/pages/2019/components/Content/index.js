a4415cdbe063655b5e062f1dfb89c74d9bf877fd
plhnk <paul.hanaoka@gmail.com>
2019-12-26 22:44:11 -0800

    parse paragraphs from yaml strings

--- src/pages/2019/components/Content/index.js
+++ src/pages/2019/components/Content/index.js
@@ -5,10 +5,17 @@ import styles from './styles.module.scss'
 const Content = ({ pretitle, title, copy, align, justify }) => {
 	return (
 		<Flex align={align} justify={justify} direction="column">
-            <Text weight="black" color="primary" className={styles.preHeader}>{pretitle}</Text>
-            <Heading className={styles.f2} color="white" level={2}>{title}</Heading>
-            <Text align={align} size="medium" className={styles.p}>{copy}</Text>
-        </Flex>
+			<Text weight="black" color="primary" className={styles.preHeader}>
+				{pretitle}
+			</Text>
+			<Heading className={styles.f2} color="white" level={2}>
+				{title}
+			</Heading>
+			{copy && copy.split("\n\n").map((paragraph, i) => (
+				<Text type="p" key={i} align={align} size="medium" className={styles.p}>{paragraph}</Text>
+                ))}
+            {/* <Text align={align} size="medium" className={styles.p}>{copy}</Text> */}
+		</Flex>
 	)
 }
 
