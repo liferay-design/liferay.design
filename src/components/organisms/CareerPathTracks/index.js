e5928abd4520b869bac55da1bc153bec189334f8
plhnk <paul.hanaoka@gmail.com>
2020-01-14 14:28:42 -0800

    change from named to default export so we could define propTypes, so we can filter by vertical

--- src/components/organisms/CareerPathTracks/index.js
+++ src/components/organisms/CareerPathTracks/index.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import { useStaticQuery, graphql } from 'gatsby'
-import { CardGrid, Text, Container } from 'components/atoms'
-import { CardDefault } from 'components/molecules'
+import PropTypes from 'prop-types'
 
-export default ({ teammate, currentPost, ...props }) => {
+export const CareerPathTracks = ({
+	vertical,
+	description,
+	summary,
+	signals,
+	...props
+}) => {
 	const data = useStaticQuery(graphql`
 		{
 			allPathTracksJson {
@@ -23,24 +28,39 @@ export default ({ teammate, currentPost, ...props }) => {
 		}
 	`)
 
-	const Tracks = data.allPathTracksJson.edges.map(({ node }) => (
-		<>
-			<h2>{node.label}</h2>
-			<p>{node.description}</p>
-			<div>
-				{node.levels.map(level => (
-					<div>
-						<p>{level.summary}</p>
-						<ul>
-							{level.signals.map(signal => (
-								<li>{signal}</li>
-							))}
-						</ul>
-					</div>
-				))}
-			</div>
-		</>
-	))
+	const Tracks = data.allPathTracksJson.edges
+		.filter(edgeItem => edgeItem.node.vertical === vertical)
+		.map(({ node }) => (
+			<>
+				<h2>{node.label}</h2>
+				{description ? <p>{node.description}</p> : null}
+				<div>
+					{summary
+						? node.levels.map(level => (
+								<div>
+									<p>{level.summary}</p>
+									{signals ? (
+										<ul>
+											{level.signals.map(signal => (
+												<li>{signal}</li>
+											))}
+										</ul>
+									) : null}
+								</div>
+						  ))
+						: null}
+				</div>
+			</>
+		))
 
 	return <div>{Tracks}</div>
 }
+
+CareerPathTracks.propTypes = {
+	vertical: PropTypes.string,
+	description: PropTypes.bool,
+	signals: PropTypes.bool,
+	summary: PropTypes.bool,
+}
+
+export default CareerPathTracks
