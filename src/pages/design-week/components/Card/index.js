a3cc7befcf96faef0c71e923c9b7b1528901eafe
plhnk <paul.hanaoka@gmail.com>
2020-01-15 10:33:18 -0800

    change Card to a class component so its easier to animate

--- src/pages/design-week/components/Card/index.js
+++ src/pages/design-week/components/Card/index.js
@@ -1,33 +1,41 @@
 import { Text, Link, AnimateIn, Flex, Button } from 'components/atoms'
 import PropTypes from 'prop-types'
-import React from 'react'
+import React, {Component} from 'react'
 import styles from './styles.module.scss'
 
-const Card = ({ location, date, ctaUrl, cta }) => (
-	<AnimateIn delay='.4s'>
-		<Flex>
-			<div>
-				<AnimateIn>
-					<Text>{location}</Text>
-				</AnimateIn>
-				<AnimateIn>
-					<Text>{date}</Text>
-				</AnimateIn>
-			</div>
-			<AnimateIn>
-				<Link to={ctaUrl}>
-					<Button>{cta}</Button>
-				</Link>
-			</AnimateIn>
-		</Flex>
-	</AnimateIn>
-)
+export class Card extends Component {
+	static propTypes = {
+		location: PropTypes.string,
+		date: PropTypes.string,
+		ctaUrl: PropTypes.string,
+		cta: PropTypes.string,
+	}
 
-Card.propTypes = {
-	location: PropTypes.string,
-	date: PropTypes.string,
-	ctaUrl: PropTypes.string,
-	cta: PropTypes.string,
+	render() {
+        const { location, date, ctaUrl, cta } = this.props
+        
+        const baseDelay = '.2'
+
+		return (
+			<AnimateIn delay={`${baseDelay}` + 's'}>
+				<Flex className={styles.wrapper}>
+					<div>
+						<AnimateIn  delay={`${baseDelay}` * 2 + 's'}>
+							<Text>{location}</Text>
+						</AnimateIn>
+						<AnimateIn delay={`${baseDelay}` * 3 + 's'}>
+							<Text>{date}</Text>
+						</AnimateIn>
+					</div>
+					<AnimateIn delay={`${baseDelay}` * 4 + 's'}>
+						<Link to={ctaUrl}>
+							<Button>{cta}</Button>
+						</Link>
+					</AnimateIn>
+				</Flex>
+			</AnimateIn>
+		)
+	}
 }
 
 export default Card
