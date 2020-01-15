2850960c094e48c61d9f4a33c395717c4d747d7e
plhnk <paul.hanaoka@gmail.com>
2020-01-15 14:03:05 -0800

    start styling Heading, create photo mask

--- src/pages/design-week/components/Heading/index.js
+++ src/pages/design-week/components/Heading/index.js
@@ -1,18 +1,19 @@
 import { Heading } from 'components/atoms'
 import PropTypes from 'prop-types'
 import React from 'react'
-// import styles from './styles.module.scss'
+import styles from './styles.module.scss'
 
 export default class DWHeading extends React.Component {
-    static propTypes = {
-        title: PropTypes.string
-    }
+	static propTypes = {
+		title: PropTypes.string,
+	}
 
-    render () {
-        const { title } = this.props
-        return (
-            <Heading>{title}</Heading>
-        )
-    }
-
+	render() {
+		const { title } = this.props
+		return (
+			<Heading className={styles.f1} level={1}>
+				{title}
+			</Heading>
+		)
+	}
 }
