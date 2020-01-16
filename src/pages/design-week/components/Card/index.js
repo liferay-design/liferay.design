38756ef86f8fa1258c457ad4408705b8cdeff626
plhnk <paul.hanaoka@gmail.com>
2020-01-15 19:14:49 -0800

    fix mysterious bug, location is not a valid prop name :|

--- src/pages/design-week/components/Card/index.js
+++ src/pages/design-week/components/Card/index.js
@@ -5,29 +5,29 @@ import styles from './styles.module.scss'
 
 export class Card extends Component {
 	static propTypes = {
-		location: PropTypes.string,
+		place: PropTypes.string,
 		date: PropTypes.string,
 		ctaUrl: PropTypes.string,
 		cta: PropTypes.string,
 	}
 
 	render() {
-		const { location, date, ctaUrl, cta } = this.props
+		const { place, date, ctaUrl, cta } = this.props
 
 		const baseDelay = '.2'
 
 		return (
 			<Flex direction="column" justify="flex-end" className={styles.wrapper}>
-				<AnimateIn delay={`${baseDelay}` + 's'}>
+				<AnimateIn delay={baseDelay * 2 + 's'}>
 					<div className={styles.details}>
-						<AnimateIn delay={`${baseDelay}` * 2 + 's'}>
-							<Heading color="mainl2" className={styles.heading} level={2}>{location}</Heading>
+						<AnimateIn delay={baseDelay * 2 + 's'}>
+							<Heading color="mainl2" className={styles.heading} level={2}>{place}</Heading>
 						</AnimateIn>
-						<AnimateIn delay={`${baseDelay}` * 3 + 's'}>
+						<AnimateIn delay={baseDelay * 2 + 's'}>
 							<Heading color='mainl5' className={styles.heading}  level={2}>{date}</Heading>
 						</AnimateIn>
 					</div>
-					<AnimateIn delay={`${baseDelay}` * 4 + 's'}>
+					<AnimateIn delay={baseDelay * 4 + 's'}>
 						<Link to={ctaUrl}>
 							<Button padding="medium" className={styles.button} size="initial">{cta}</Button>
 						</Link>
