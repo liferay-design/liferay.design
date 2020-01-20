535b6bffafcfcc3274bfa57b2ea4f128bfda623f
plhnk <paul.hanaoka@gmail.com>
2020-01-20 11:45:52 -0800

    change rendering method of design-week so @ryconn888 doesnt have to refresh so many times

--- src/pages/design-week/index.js
+++ src/pages/design-week/index.js
@@ -9,62 +9,63 @@ import { Card, Heading, LoadingAnimation } from 'pages/design-week/components/_i
 import styles from './styles.module.scss'
 import { spacing } from 'theme'
 
-const DesignWeek = () => {
-	const baseDelay = '1.6'
+export default class DesignWeek extends React.Component {
 
-	function getRandomBgImage(max) {
-		return Math.floor(Math.random() * Math.floor(max))
+	render() {
+		const baseDelay = '1.6'
+
+		function getRandomBgImage(max) {
+			return Math.floor(Math.random() * Math.floor(max))
+		}
+
+		const randomImage = getRandomBgImage(6)
+
+		const bgImage = '/images/design-week/bg-' + randomImage + '.jpg'
+
+		return (
+			<>
+				<SEO
+					pageTitle="Design Week 2020 | Liferay Design"
+					description="Liferay's Design Department Gathering"
+					previewImage="/images/design-week/open-graph.png"
+					twitterHandle="@liferaydesign"
+				/>
+				<LoadingAnimation size={spacing.xLarge} />
+
+				<Flex className={styles.page}>
+					<Image src={bgImage} className={styles.bgImage} />
+					<AnimateIn delay={baseDelay * 1.05 + 's'}>
+						<Link to="/">
+							<Icon
+								name="liferayDesicon"
+								height="2rem"
+								width="2rem"
+								fill="white"
+								margin="0 0 2rem -.2rem"
+								className={styles.navIcon}
+							/>
+						</Link>
+					</AnimateIn>
+					<Grid className={styles.grid}>
+						<Grid.Item area="sidebar" className={styles.sidebar}>
+							<Card
+								place="Los Angeles, CA"
+								date="Mar 23—27"
+								cta="RSVP"
+								ctaUrl="https://forms.gle/PSkzN2vNmwjLz2B37"
+								delay={baseDelay}
+							/>
+						</Grid.Item>
+						<Grid.Item area="hero" style={{ zIndex: '2' }}>
+							<Heading
+								bgImage={bgImage}
+								delay={baseDelay}
+								title="Design Week"
+							/>
+						</Grid.Item>
+					</Grid>
+				</Flex>
+			</>
+		)
 	}
-
-	const randomImage = getRandomBgImage(6)
-
-	const bgImage = '/images/design-week/bg-' + randomImage + '.jpg'
-
-	return (
-		<>
-			<SEO
-				pageTitle="Design Week 2020 | Liferay Design"
-				description="Liferay's Design Department Gathering"
-				previewImage="/images/design-week/open-graph.png"
-				twitterHandle="@liferaydesign"
-			/>
-			<LoadingAnimation size={spacing.xLarge} />
-
-			<Flex className={styles.page}>
-				<Image src={bgImage} className={styles.bgImage} />
-				<AnimateIn delay={baseDelay * 1.05 + 's'}>
-					<Link to="/">
-						<Icon
-							name="liferayDesicon"
-							height="2rem"
-							width="2rem"
-							fill="white"
-							margin="0 0 2rem -.2rem"
-							className={styles.navIcon}
-						/>
-					</Link>
-				</AnimateIn>
-				<Grid className={styles.grid}>
-					<Grid.Item area="sidebar" className={styles.sidebar}>
-						<Card
-							place="Los Angeles, CA"
-							date="Mar 23—27"
-							cta="RSVP"
-							ctaUrl="https://forms.gle/PSkzN2vNmwjLz2B37"
-							delay={baseDelay}
-						/>
-					</Grid.Item>
-					<Grid.Item area="hero" style={{zIndex:'2'}}>
-						<Heading
-							bgImage={bgImage}
-							delay={baseDelay}
-							title="Design Week"
-						/>
-					</Grid.Item>
-				</Grid>
-			</Flex>
-		</>
-	)
 }
-
-export default DesignWeek
