53dc1164e805915cf13fec241bbd7cdeaf54993a
plhnk <paul.hanaoka@gmail.com>
2020-01-24 09:16:36 -0800

    update Newsletter portion of MainLayout

--- src/components/templates/MainLayout/index.js
+++ src/components/templates/MainLayout/index.js
@@ -1,7 +1,7 @@
 import { Footer, Navbar } from 'components/organisms'
 import { FormNewsletter, FancyFooter } from 'components/molecules'
 import React from 'react'
-import { Container, Heading, Text } from 'components/atoms';
+import { Link, Container, Heading, Text, Icon } from 'components/atoms'
 
 export default ({ children, section }) => (
 	<>
@@ -10,9 +10,16 @@ export default ({ children, section }) => (
 		{children}
 
 		<Container padding="6rem 0 0">
-			<Heading align='center' color='white' level={2}>Sign up for our newsletter!</Heading>
-			<Text align='center' color="lightGrey" margin='base'>No mystery meat here! Once a month we share links, articles, job listings, and more.</Text>
-			<FormNewsletter placeholderText='Your Email Address' submitText='Subscribe' />
+			<Heading align="center" color="white" level={2}>
+				Sign up for our monthly newsletter!
+			</Heading>
+			<Link to="/newsletter">
+				<Text align="center" color="lightGrey" margin="base">
+					Read past issues
+					<Icon fill="lightGrey" margin='.2em 0 0 1em' name="rightArrow" />
+				</Text>
+			</Link>
+			<FormNewsletter placeholderText="Your Email Address" submitText="Subscribe" />
 		</Container>
 		<Footer />
 		<FancyFooter />
