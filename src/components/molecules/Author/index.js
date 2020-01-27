ad5cbd334a790e11518d13edca56f00f268cb32d
plhnk <paul.hanaoka@gmail.com>
2020-01-27 12:43:02 -0800

    refactor <Author> component per updates to authors.yaml

--- src/components/molecules/Author/index.js
+++ src/components/molecules/Author/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useStaticQuery, graphql } from 'gatsby'
 import { Image, Link, Text, Flex } from 'components/atoms'
+const { kebabCase } = require(`lodash`)
 
 export default ( {id, guest, ...props} ) => {
 	const data = useStaticQuery(graphql`
@@ -9,31 +10,29 @@ export default ( {id, guest, ...props} ) => {
                 edges {
                     node {
                         id
-                        slug
-                        headshot
                     }
                 }
             }
         }
 	`)
 
+	const imagePath = '/images/headshots/'
+
 	const author = data.allAuthorsYaml.edges
-        .filter(edges => edges.node.id === id)
-        .map(({ node }) => (
+		.filter(edges => edges.node.id === id)
+		.map(({ node }) => (
 			<Flex {...props} align="center">
-				<Link to={'/team/' + `${node.slug}`}> 
-					<div
-						style={{marginRight:'.5rem', width:'3rem'}}
-					>
-						<Image circle src={node.headshot} />
+				<Link to={'/team/' + `${kebabCase(node.id)}`}>
+					<div style={{ marginRight: '.5rem', width: '3rem' }}>
+						<Image circle src={imagePath + kebabCase(node.id)} />
 					</div>
 				</Link>
-				<Text size="medium" color="grey" >
-	 				by {''}
-	 				<Link to={'/team/' + `${node.slug}`}>{id}</Link>
-	 			</Text>
+				<Text size="medium" color="grey">
+					by {''}
+					<Link to={'/team/' + `${kebabCase(node.id)}`}>{id}</Link>
+				</Text>
 			</Flex>
-        ))
+		))
 
 	return (
         <div>{author}</div>
