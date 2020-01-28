da3b3510f7888d29e3e08e74bf5f356e9704d36b
plhnk <paul.hanaoka@gmail.com>
2020-01-27 22:18:12 -0800

    rewrite createPages using Promise.all --> try to have site not require API keys

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,6 @@
 const path = require(`path`)
 const { createFilePath } = require(`gatsby-source-filesystem`)
-const _ = require("lodash")
+const _ = require('lodash')
 const moment = require(`moment`)
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
@@ -20,108 +20,117 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
 exports.createPages = ({ actions, graphql }) => {
 	const { createPage } = actions
 
-	return new Promise((resolve, reject) => {
-		resolve(
-			graphql(`
-				query {
-					markdown: allMdx {
-						edges {
-							node {
-								id
-								fields {
-									slug
-								}
-								frontmatter {
-									tags
-								}
-								body
-							}
+	const markdown = graphql(`
+		{
+			markdown: allMdx {
+				edges {
+					node {
+						id
+						fields {
+							slug
 						}
-					}
-					mailchimp: allNewsletters(
-						filter: {
-							settings: { title: { regex: "/Newsletter/" } }
-							emails_sent: { gt: 20 }
+						frontmatter {
+							tags
 						}
-					) {
-						edges {
-							node {
-								archive_url
-								archive_html
-								settings {
-									title
-									subject_line
-									preview_text
-								}
-								emails_sent
-								send_time
-							}
-						}
+						body
 					}
 				}
-			`).then(({ data, errors }) => {
-				if (errors) {
-					console.log('Error creating pages in `createPages` call ==>', errors)
-					reject(errors)
-				}
+			}
+		}
+	`).then(({ data, errors }) => {
+		if (errors) {
+			console.log('Error creating markdown pages in `createPages` call ==>', errors)
+			reject(errors)
+		}
 
-				const pages = data.markdown.edges
-				pages.forEach(({ node }) => {
-					const template = node.fields.slug.split('/')[1]
+		const pages = data.markdown.edges
+		pages.forEach(({ node }) => {
+			const template = node.fields.slug.split('/')[1]
 
-					const templateFile = path.resolve(
-						`./src/components/templates/${capFirstLetter(template)}/index.js`,
-					)
+			const templateFile = path.resolve(
+				`./src/components/templates/${capFirstLetter(template)}/index.js`,
+			)
 
-					createPage({
-						path: node.fields.slug,
-						component: templateFile,
-						context: {
-							slug: node.fields.slug,
-						},
-					})
-				})
-				const newsletters = data.mailchimp.edges
-				newsletters.forEach(({ node }) => {
-					const templateFile = path.resolve(
-						`./src/components/templates/Newsletters/index.js`,
-					)
-
-					const slug = `${moment(node.send_time).format('YYYY-MM')}`
-
-					createPage({
-						path: '/newsletter/' + slug,
-						component: templateFile,
-						context: {
-							slug: slug,
-							send_time: node.send_time,
-						},
-					})
-				})
-				const tagTemplate = path.resolve('src/components/templates/Tags/index.js')
-				// Tag pages:
-				let tags = []
-				// Iterate through each post, putting all found tags into `tags`
-				_.each(pages, edge => {
-					if (_.get(edge, 'node.frontmatter.tags')) {
-						tags = tags.concat(edge.node.frontmatter.tags)
-					}
-				})
-				// Eliminate duplicate tags
-				tags = _.uniq(tags)
-				// Make tag pages
-				tags.forEach(tag => {
-					createPage({
-						path: `/tags/${_.kebabCase(tag)}/`,
-						component: tagTemplate,
-						context: {
-							tag,
-						},
-					})
-				})
-			}),
-		)
+			createPage({
+				path: node.fields.slug,
+				component: templateFile,
+				context: {
+					slug: node.fields.slug,
+				},
+			})
+		})
+		const tagTemplate = path.resolve('src/components/templates/Tags/index.js')
+		// Tag pages:
+		let tags = []
+		// Iterate through each post, putting all found tags into `tags`
+		_.each(pages, edge => {
+			if (_.get(edge, 'node.frontmatter.tags')) {
+				tags = tags.concat(edge.node.frontmatter.tags)
+			}
+		})
+		// Eliminate duplicate tags
+		tags = _.uniq(tags)
+		// Make tag pages
+		tags.forEach(tag => {
+			createPage({
+				path: `/tags/${_.kebabCase(tag)}/`,
+				component: tagTemplate,
+				context: {
+					tag,
+				},
+			})
+		})
 	})
+
+	const newsletters = graphql(`
+		{
+			mailchimp: allNewsletters(
+				filter: {
+					settings: { title: { regex: "/Newsletter/" } }
+					emails_sent: { gt: 20 }
+				}
+			) {
+				edges {
+					node {
+						archive_url
+						archive_html
+						settings {
+							title
+							subject_line
+							preview_text
+						}
+						emails_sent
+						send_time
+					}
+				}
+			}
+		}
+	`).then(({ data, errors }) => {
+		if (errors) {
+			console.log('Error creating newsletter pages in `createPages` call ==>', errors)
+			reject(errors)
+		}
+
+		const newsletters = data.mailchimp.edges
+		newsletters.forEach(({ node }) => {
+			const templateFile = path.resolve(
+				`./src/components/templates/Newsletters/index.js`,
+			)
+
+			const slug = `${moment(node.send_time).format('YYYY-MM')}`
+
+			createPage({
+				path: '/newsletter/' + slug,
+				component: templateFile,
+				context: {
+					slug: slug,
+					send_time: node.send_time,
+				},
+			})
+		})
+	})
+
+	return Promise.all([markdown, newsletters])
 }
 
 function capFirstLetter(string) {
