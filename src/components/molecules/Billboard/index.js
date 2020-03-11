9623b2e9caf844c1d9e823422d9499f469eeca3b
plhnk <paul.hanaoka@gmail.com>
2020-03-11 15:35:19 -0700

    feature: make Billboard component mobile friendly

--- src/components/molecules/Billboard/index.js
+++ src/components/molecules/Billboard/index.js
@@ -3,11 +3,18 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import { Grid } from 'reakit'
 import styles from './styles.module.scss'
+import {useMediaQuery} from 'react-responsive'
 
 const Billboard = ({ image, heading, Subtitle, background }) => {
+	const gridTemplate = `"gutterL icon copy gutterR" auto / 1fr 20rem 30rem 1fr`
+	const gridTemplateMobile = `"icon" auto
+								 "copy" auto / 1fr`
+
+	const isMobile = useMediaQuery({query: '(max-width: 768px)'})
+
 	return (
 		<section className={styles.wrapper} style={{ background: `${background}` }}>
-			<Grid templateColumns="1fr 20rem 30rem 1fr" templateAreas="'gutterL icon copy gutterR'">
+			<Grid template={isMobile ? gridTemplateMobile : gridTemplate }>
 				<Icon className={styles.icon} name={image} />
 
 				<div className={styles.copy}>
