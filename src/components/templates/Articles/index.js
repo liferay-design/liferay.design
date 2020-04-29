b33dfba13671bf3baa97892047ceba2208a0100b
plhnk <paul.hanaoka@gmail.com>
2020-04-28 23:02:00 -0700

    fix Article template overflow that resulted from fix for #660

--- src/components/templates/Articles/index.js
+++ src/components/templates/Articles/index.js
@@ -8,6 +8,9 @@ import React, { Component } from 'react'
 import styles from './styles.module.scss'
 import { colors } from 'theme'
 import { makeAuthorSlug } from 'utils'
+import { Grid } from 'reakit'
+import MediaQuery from 'react-responsive'
+import { Media } from 'react-md'
 
 export default class Articles extends Component {
 	render() {
@@ -42,59 +45,78 @@ export default class Articles extends Component {
 				/>
 				<Navbar section="Articles" />
 				<Container>
-					<Tags tags={post.frontmatter.tags} />
-					<Flex justify="center" align="center" className={styles.banner}>
-						<h1>
-							{post.frontmatter.title}{' '}
-							<span>
-								by{' '}
-								<Link
-									style={{
-										color: colors.primaryl3,
-										textDecoration: 'underline',
-										textDecorationColor: colors.primary,
-									}}
-									to={withPrefix(
-										'/team/' + `${makeAuthorSlug(post.frontmatter.author.id)}`,
-									)}
-								>
-									{post.frontmatter.author.id}
-								</Link>
-								{post.frontmatter.contributors ? (
-									<>
-										{' '} and {' '} 
-											{post.frontmatter.contributors.map(i => (
-<Link
-											style={{
-												color: colors.primaryl3,
-												textDecoration: 'underline',
-												textDecorationColor: colors.primary,
-											}}
-											to={withPrefix(
-												'/team/' +
-													`${makeAuthorSlug(i.id)}`,
-											)}
-										>
-											{i.id}
-										</Link>
-										))}
-									</>
-								) : null}
-								&nbsp;on{' '}
-								{moment(post.frontmatter.date).format('MMMM DD, YYYY')}
-							</span>
-						</h1>
-						<div className={styles.role}>
-							<h2>{post.timeToRead} Min Read</h2>
-						</div>
-					</Flex>
+					<MediaQuery maxWidth={767}>
+					{matches => {
+						let gridTemplate = matches
+							? `	"c v" auto
+								"i i" auto / 1fr 1rem
+							`
+							: `	"c c v" auto
+								"l i r" auto
+								"l i r" auto / 1fr 768px 1fr`
+						return (
+							<Grid template={gridTemplate} className={styles.banner}>
+								<Grid.Item area='c'>
+									{post.frontmatter.tags ? (
+										<Tags tags={post.frontmatter.tags} />
+									) : null}
+									<h1>
+										{post.frontmatter.title}{' '}
+										<span>
+											by{' '}
+											<Link
+												style={{
+													color: colors.primaryl3,
+													textDecoration: 'underline',
+													textDecorationColor: colors.primary,
+												}}
+												to={withPrefix(
+													'/team/' +
+														`${makeAuthorSlug(
+															post.frontmatter.author.id,
+														)}`,
+												)}
+											>
+												{post.frontmatter.author.id}
+											</Link>
+											{post.frontmatter.contributors ? (
+												<>
+													{' '}
+													and{' '}
+													{post.frontmatter.contributors.map(i => (
+														<Link
+															style={{
+																color: colors.primaryl3,
+																textDecoration: 'underline',
+																textDecorationColor: colors.primary,
+															}}
+															to={withPrefix(
+																'/team/' + `${makeAuthorSlug(i.id)}`,
+															)}
+														>
+															{i.id}
+														</Link>
+													))}
+												</>
+											) : null}
+											&nbsp;on{' '}
+											{moment(post.frontmatter.date).format('MMMM DD, YYYY')}
+										</span>
+									</h1>
+								</Grid.Item>
+								<Grid.Item area='v' alignSelf='center' justifySelf='center' className={styles.role}>
+									<h2>{post.timeToRead} Min Read</h2>
+								</Grid.Item>
+								<Grid.Item area='i' className={styles.featuredImage}>
+									<Image
+										alt={'featured image for ' + `${post.frontmatter.title}`}
+										src={post.frontmatter.featuredImage}
+									/>
+								</Grid.Item>
+							</Grid>)}}
+					</MediaQuery>
 				</Container>
 				<div className={styles.markdownContainer}>
-					<Image
-						alt={'featured image for ' + `${post.frontmatter.title}`}
-						className={styles.featuredImage}
-						src={post.frontmatter.featuredImage}
-					/>
 					<div className={styles.wrapper}>
 						<GlobalMdx>
 							<MDXRenderer>{post.body}</MDXRenderer>
