5f94c35fffc539fa0c459299ce92906f61c94498
plhnk <paul.hanaoka@gmail.com>
2020-07-22 13:59:37 -0700

    change research card page implementation

---
+++ src/pages/resources/research/index.js
@@ -0,0 +1,77 @@
+import { CardGrid, Container, Heading, AnimateIn, Icon } from 'components/atoms'
+import { CardDefault, SEO } from 'components/molecules'
+import { MainLayout } from 'components/templates'
+import { graphql } from 'gatsby'
+import React from 'react'
+import { slugToTitle } from 'utils'
+import { MDXRenderer } from 'gatsby-plugin-mdx'
+import styles from './styles.module.scss'
+import { fontSizes } from 'theme/'
+
+
+export default ({ data }) => {
+
+	return (
+		<MainLayout section="Resources">
+			<SEO
+				description="Liferay Design Research"
+				pageTitle="Liferay.Design | Research Resources"
+			/>
+			<Container background="white" heading="Research Methods" color="black">
+				<CardGrid>
+					{data.allMdx.edges.map(({ node }, index) => (
+						<AnimateIn key={node.id} delay={`${index}` * 0.1 + 's'}>
+							<article>
+								<header>
+									<Heading
+										level={1}
+										additionalStyles={{ fontSize: fontSizes.medium }}
+									>
+										{slugToTitle(
+											'/resources/research/',
+											node.fields.slug,
+										)}
+									</Heading>
+									<Icon
+										name={slugToTitle(
+											'/resources/research/',
+											node.fields.slug,
+										)}
+									/>
+								</header>
+								<section>
+									<Heading
+										level={2}
+										additionalStyles={{ fontSize: fontSizes.base }}
+									>
+										{node.frontmatter.subtitle}
+									</Heading>
+									<MDXRenderer>{node.body}</MDXRenderer>
+								</section>
+							</article>
+						</AnimateIn>
+					))}
+				</CardGrid>
+			</Container>
+		</MainLayout>
+	)
+}
+
+export const query = graphql`
+	{
+		allMdx(filter: { fileAbsolutePath: { regex: "/(resources/research)/" } }) {
+			edges {
+				node {
+					id
+					frontmatter {
+						subtitle
+					}
+					body
+					fields {
+						slug
+					}
+				}
+			}
+		}
+	}
+`
