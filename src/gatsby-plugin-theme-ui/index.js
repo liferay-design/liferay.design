e4f230a9f624dc760f1f9a6441045a1c69a9e8a0
plhnk <paul.hanaoka@gmail.com>
2020-07-23 15:22:26 -0700

    remove reakit from all components

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -2,6 +2,13 @@ export default {
 	colors: {
 		text: 'inherit',
 		background: 'inherit',
+		primary: '#0B5FFF',
+	},
+	buttons: {
+		primary: {
+			color: 'white',
+			bg: 'primary',
+		},
 	},
 	grids: {
 		twoCards: {
@@ -17,6 +24,14 @@ export default {
 				'"nav" 8rem "main" 1fr / 1fr',
 				null,
 				'"sidebar main" auto / 20rem 1fr',
+			],
+			gap: 0,
+		},
+		post: {
+			gridTemplate: [
+				'"c v" auto "i i" auto / 1fr 1rem',
+				null,
+				'"c c v" auto "l i r" auto "l i r" auto / 1fr 768px 1fr',
 			],
 			gap: 0,
 		},
