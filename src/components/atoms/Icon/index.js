8bda52d1f8bbfb285f412d82f84749308f7cdf28
plhnk <paul.hanaoka@gmail.com>
2020-08-04 16:23:03 -0700

    remove `rotate` prop from <Icon>

--- src/components/atoms/Icon/index.js
+++ src/components/atoms/Icon/index.js
@@ -3,12 +3,7 @@ import { jsx } from 'theme-ui'
 import PropTypes from 'prop-types'
 import IconsObject from './icons'
 
-const Icon = ({ className, name, rotate, ...props }) => {
-	if (rotate) {
-		styles.transform = `rotate(${rotate}deg)`
-		styles.transition = 'transform 0.4s'
-	}
-
+const Icon = ({ className, name, ...props }) => {
 	const DynamicIcon = IconsObject[name]
 
 	if (!DynamicIcon) {
@@ -25,7 +20,6 @@ const Icon = ({ className, name, rotate, ...props }) => {
 Icon.propTypes = {
 	className: PropTypes.string,
 	name: PropTypes.string.isRequired,
-	rotate: PropTypes.number,
 }
 
 export default Icon
