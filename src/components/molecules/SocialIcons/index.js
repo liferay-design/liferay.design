bdd1575ddc43d9857da925a510dc3bfd1da1bb21
plhnk <paul.hanaoka@gmail.com>
2020-08-17 21:54:55 -0700

    feature: add Figma community link to footer (#770 and #754)

--- src/components/molecules/SocialIcons/index.js
+++ src/components/molecules/SocialIcons/index.js
@@ -3,50 +3,49 @@ import React from 'react'
 import styles from './styles.module.scss'
 
 const SocialIcons = () => {
+	const socials = [
+		{
+			url: 'https://www.dribbble.com/liferay',
+			name: 'dribbble',
+		},
+		{
+			url: 'https://figma.com/@liferay',
+			name: 'figma',
+		},
+		{
+			url: 'https://github.com/liferay-design/',
+			name: 'github',
+		},
+		{
+			url: 'https://www.instagram.com/liferaydesign/',
+			name: 'instagram',
+		},
+		{
+			url: 'https://www.medium.com/liferaydesign',
+			name: 'medium',
+		},
+		{
+			url: 'https://twitter.com/liferaydesign',
+			name: 'twitter',
+		},
+		{
+			url:
+				'https://www.youtube.com/playlist?list=PLFNUy_HrVhPsBRvotF2sj2sBaTXlIlNgm',
+			name: 'youtube',
+		},
+	]
+
 	return (
 		<Flex className={styles.social} justify="space-between">
-			<Link
-				aria-label="Liferay.Design on dribbble"
-				to="https://www.dribbble.com/liferay"
-			>
-				<Icon name="dribbble" />
-			</Link>
-
-			<Link
-				aria-label="Liferay.Design on github"
-				to="https://github.com/liferay-design/"
-			>
-				<Icon name="github" />
-			</Link>
-
-			<Link
-				aria-label="Liferay.Design on instagram"
-				to="https://www.instagram.com/liferaydesign/"
-			>
-				<Icon name="instagram" />
-			</Link>
-
-			<Link
-				aria-label="Liferay.Design on medium"
-				to="https://www.medium.com/liferaydesign"
-			>
-				<Icon name="medium" />
-			</Link>
-
-			<Link
-				aria-label="Liferay.Design on twitter"
-				to="https://twitter.com/liferaydesign"
-			>
-				<Icon name="twitter" />
-			</Link>
-
-			<Link
-				aria-label="Liferay.Design on youtube"
-				to="https://www.youtube.com/playlist?list=PLFNUy_HrVhPsBRvotF2sj2sBaTXlIlNgm"
-			>
-				<Icon name="youtube" />
-			</Link>
-			
+			{socials.map(item => (
+				<Link
+					key={item.name}
+					aria-label={'Liferay.Design on ' + item.name}
+					to={item.url}
+				>
+					<Icon sx={{ height: '1.5rem', width: '1.5rem' }} name={item.name} />
+				</Link>
+			))}
 		</Flex>
 	)
 }
