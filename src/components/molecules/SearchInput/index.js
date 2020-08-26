1c50d7a32c46140df8c36ee33529501377601922
plhnk <paul.hanaoka@gmail.com>
2020-08-25 19:31:03 -0700

    move inline styles to module so :global styles override algolia defaults

--- src/components/molecules/SearchInput/index.js
+++ src/components/molecules/SearchInput/index.js
@@ -5,20 +5,12 @@ import { Icon } from 'components/atoms'
 import PropTypes from 'prop-types'
 import styles from './styles.module.scss'
 
-export default function SearchInput({ id, ...props }) {
-	// const imagePath = '/images/headshots/'
-
+export default function SearchInput({ id }) {
 	return (
 		<Flex
+			className={styles.searchInput}
 			sx={{
-				mt: [0, null, '-3rem'],
-				width: '100%',
-				position: 'sticky',
-				top: '1rem',
-				zIndex: '1000',
-				boxShadow:
-					'0 .5rem 2rem #f7f8f9, 0 1rem 2rem #f7f8f9, 0 2rem 2rem #f7f8f9, 0 -2rem 0 #f7f8f9',
-				'& > span': { width: '100%' },
+				mt: [0, null, '-3rem']
 			}}
 		>
 			<Input id={id} placeholder="Search" />
