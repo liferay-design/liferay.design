afffda0ca832ae2792180a6881c50e42953de13e
plhnk <paul.hanaoka@gmail.com>
2020-09-15 23:52:30 -0700

    feature: first draft of PreviousNext in articles

--- src/components/organisms/PreviousNext.js
+++ src/components/organisms/PreviousNext.js
@@ -1,10 +1,14 @@
 /** @jsx jsx */
 
-import { jsx, Flex, Box, Text } from 'theme-ui'
+import { jsx, Styled, Flex, Box, Text } from 'theme-ui'
 import { useStaticQuery, graphql } from 'gatsby'
 import { Link } from 'components/atoms'
+import { slugToSection } from 'utils'
 
 export default function PreviousNext({ current, section, ...props }) {
+
+    const H3 = Styled.h3
+
 	const data = useStaticQuery(graphql`
 		{
 			allMdx(
@@ -43,18 +47,22 @@ export default function PreviousNext({ current, section, ...props }) {
 	const next = Posts[0].next
 
 	return (
-		<Flex sx={{ justifyContent: 'space-between' }}>
-			<Box>
-				<Text sx={{ variant: 'caps' }}>Previous</Text>
-				<Link to={previous.fields.slug}>
-					<div>{previous.frontmatter.title}</div>
-				</Link>
-			</Box>
-			{next.fields.slug == section ? (
+		<Flex sx={{ justifyContent: 'space-between', my: 5 }}>
+			{previous && slugToSection(previous.fields.slug) === section ? (
+				<Box>
+					<Text sx={{ variant: 'text.caps' }}>Previous</Text>
+					<Link to={previous.fields.slug}>
+						<H3>{previous.frontmatter.title}</H3>
+					</Link>
+				</Box>
+			) : (
+				<div></div>
+			)}
+			{next && slugToSection(next.fields.slug) === section ? (
 				<Box sx={{ textAlign: 'right' }}>
-					<Text sx={{ variant: 'caps' }}>Next</Text>
+					<Text sx={{ variant: 'text.caps' }}>Next</Text>
 					<Link sx={{}} to={next.fields.slug}>
-						<div>{next.frontmatter.title}</div>
+						<H3>{next.frontmatter.title}</H3>
 					</Link>
 				</Box>
 			) : (
