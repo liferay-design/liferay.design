1d783bfdde0a5b954a028b983898543187dac9fb
plhnk <paul.hanaoka@gmail.com>
2020-09-16 15:48:07 -0700

    use index instead of keys for key

--- src/components/atoms/Hotkey.js
+++ src/components/atoms/Hotkey.js
@@ -3,15 +3,11 @@
 import { jsx, Flex, Text } from 'theme-ui'
 
 export default function Hotkey({ id, keys, ...props }) {
-	const keyArray = keys.map(item => (
-		<Text key={id} {...props} sx={{ variant: 'text.hotkey' }}>
+	const keyArray = keys.map((item, index) => (
+		<Text key={index} {...props} sx={{ variant: 'text.hotkey' }}>
 			{item}
 		</Text>
 	))
 
-	return (
-		<Flex id={id}>
-			{keyArray}
-		</Flex>
-	)
+	return <Flex id={id}>{keyArray}</Flex>
 }
