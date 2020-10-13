caa999a8c7f1e4dbc7f78a79184a27c350f4dc86
plhnk <paul.hanaoka@gmail.com>
2020-10-12 22:31:11 -0700

    feature: add previous/next links to newsletter archive

--- src/components/templates/Newsletters/index.js
+++ src/components/templates/Newsletters/index.js
@@ -1,3 +1,6 @@
+/** @jsx jsx */
+
+import { jsx } from 'theme-ui'
 import { graphql } from 'gatsby'
 import React, { Component } from 'react'
 import { Link, Icon } from 'components/atoms'
@@ -11,15 +14,52 @@ export default class Newsletters extends Component {
 	}
 
 	render() {
-		const data = this.props.data.newsletters
+		const newsletter = this.props.data.newsletter.edges[0]
+		const prevNext = this.props.data.prevNext.edges.filter(
+			item => item.node.send_time === newsletter.node.send_time,
+		)
+
+		const links = prevNext[0]
 
 		return (
 			<>
-				<Link style={{color:'white',position:'absolute',padding:'2em', left:'2em',top:'0'}} to="/newsletter">
-					<Icon margin="0 1em" name='leftArrow'/>
+				<Link
+					sx={{
+						variant: 'links.nice',
+						color: 'white',
+						top: '0',
+					}}
+					to="/newsletter"
+				>
+					<Icon margin="0 1em" name="leftArrow" />
 					All Newsletters
 				</Link>
-				<div style={{marginTop:'-66px'}} dangerouslySetInnerHTML={{ __html: data.archive_html }} />
+				<div
+					sx={{ marginTop: '-66px' }}
+					dangerouslySetInnerHTML={{ __html: newsletter.node.archive_html }}
+				/>
+				{links.previous ? (
+					<Link
+						sx={{ variant: 'links.nice' }}
+						to={'/newsletter/' + links.previous.send_time}
+					>
+						<Icon margin="0 1em" name="leftArrow" />
+						Previous Issue
+					</Link>
+				) : null}
+				{links.next ? (
+					<Link
+						sx={{
+							variant: 'links.nice',
+							left: 'initial',
+							right: '2em',
+						}}
+						to={'/newsletter/' + links.next.send_time}
+					>
+						Next Issue
+						<Icon margin="0 1em" name="rightArrow" />
+					</Link>
+				) : null}
 			</>
 		)
 	}
@@ -27,8 +67,44 @@ export default class Newsletters extends Component {
 
 export const pageQuery = graphql`
 	query($send_time: Date) {
-		newsletters(send_time: { eq: $send_time }) {
-			archive_html
+		newsletter: allNewsletters(
+			sort: { order: ASC, fields: send_time }
+			filter: {
+				settings: { title: { regex: "/Newsletter/" } }
+				send_time: { eq: $send_time }
+			}
+		) {
+			edges {
+				node {
+					archive_html
+					send_time(formatString: "YYYY-MM")
+				}
+			}
+		}
+		prevNext: allNewsletters(
+			sort: { order: ASC, fields: send_time }
+			filter: {
+				send_time: { ne: "" }
+				settings: { title: { regex: "/Newsletter/" } }
+			}
+		) {
+			edges {
+				node {
+					send_time(formatString: "YYYY-MM")
+				}
+				next {
+					send_time(formatString: "YYYY-MM")
+					settings {
+						title
+					}
+				}
+				previous {
+					send_time(formatString: "YYYY-MM")
+					settings {
+						title
+					}
+				}
+			}
 		}
 	}
 `
