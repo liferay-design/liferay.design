738878542663318d0f016e96e60581e3c675d0c4
plhnk <paul.hanaoka@gmail.com>
2020-10-14 23:29:20 -0700

    feature: finalize DW2.0 page

--- src/pages/design-week/components/Heading/index.js
+++ src/pages/design-week/components/Heading/index.js
@@ -1,31 +1,48 @@
 /** @jsx jsx */
 
-import { jsx, Flex } from 'theme-ui'
+import { jsx } from 'theme-ui'
 
-import { Heading, AnimateIn } from 'components/atoms'
+import { AnimateIn } from 'components/atoms'
 import PropTypes from 'prop-types'
 import React from 'react'
-import styles from './styles.module.scss'
 
 export default class DWHeading extends React.Component {
 	static propTypes = {
 		title: PropTypes.string,
 		delay: PropTypes.string,
-		bgImage: PropTypes.string,
-	}
-
-	componentDidMount() {
-		let page = document.documentElement
-		page.addEventListener('mousemove', e => {
-			let ow = page.offsetWidth
-			let oh = page.offsetHeight
-			page.style.setProperty('--mouseX', 16 + (e.clientX * 16) / ow + '%')
-			page.style.setProperty('--mouseY', 48 + (e.clientY * 10) / oh + '%')
-		})
 	}
 
 	render() {
-		const { delay, title, bgImage } = this.props
+		const { delay, title } = this.props
+
+		const titleArray = title.split('')
+
+		const top = [
+			'0%', // D
+			'32%', // E
+			'8%', // S
+			'28%', // I
+			'4%', // G
+			'34%', // N
+			'0%', // ""
+			'56%', // W
+			'64%', // E
+			'48%', // E
+			'74%', // K
+		]
+		const left = [
+			'4%', // D
+			'20%', // E
+			'36%', // S
+			'48%', // I
+			'76%', // G
+			'84%', // N
+			'0%', // ""
+			'4%', // W
+			'24%', // E
+			'58%', // E
+			'80%', // K
+		]
 
 		return (
 			<AnimateIn
@@ -34,24 +51,29 @@ export default class DWHeading extends React.Component {
 					position: 'relative',
 					width: '100%',
 					height: ['100%', null, '100vh'],
-					'> span': {
-						position: 'absolute',
-						fontSize: '20vmin',
-						fontWeight: 'heading',
-						color: 'white',
-					},
 				}}
 			>
-				<span sx={{ top: '0%', left: '4%' }}>D</span>
-				<span sx={{ top: '8%', left: '36%' }}>E</span>
-				<span sx={{ top: '4%', left: '76%' }}>S</span>
-				<span sx={{ top: '32%', left: '20%' }}>I</span>
-				<span sx={{ top: '28%', left: '48%' }}>G</span>
-				<span sx={{ top: '34%', left: '84%' }}>N</span>
-				<span sx={{ top: '56%', left: '4%' }}>W</span>
-				<span sx={{ top: '64%', left: '24%' }}>E</span>
-				<span sx={{ top: '48%', left: '58%' }}>E</span>
-				<span sx={{ top: '74%', left: '80%' }}>K</span>
+				{titleArray.map((item, index) => (
+					<AnimateIn
+						sx={{
+							top: `${top[index]}`,
+							left: `${left[index]}`,
+							position: 'absolute',
+						}}
+						delay={index * 0.1 + +delay - .2 + 's'}
+					>
+						<span
+							sx={{
+								variant: 'text.caps',
+								fontSize: '20vmin',
+								fontWeight: 'heading',
+								color: 'white',
+							}}
+						>
+							{item}
+						</span>
+					</AnimateIn>
+				))}
 			</AnimateIn>
 		)
 	}
