c134a7ff12e3257a8ddab5f87882610aebc3842b
plhnk <paul.hanaoka@gmail.com>
2020-10-16 16:34:04 -0700

    update hotkey article per @victorvalle

--- src/components/molecules/GlobalMdx/index.js
+++ src/components/molecules/GlobalMdx/index.js
@@ -1,12 +1,15 @@
+/** @jsx jsx */
+
+import { jsx, Flex, } from 'theme-ui'
 import React from 'react'
 import { MDXProvider } from '@mdx-js/react'
 import { Image } from 'components/atoms'
 import { DoDont } from 'components/molecules'
 
-const shortcodes = { Image, DoDont }
+const shortcodes = { Image, DoDont, Flex }
 
 export default ({ children }) => (
-	<div>
+	<>
 		<MDXProvider components={shortcodes}>{children}</MDXProvider>
-	</div>
+	</>
 )
