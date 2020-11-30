eab9a6bc1a6ff9234f60232a4ce6a1bcea3f617f
Abel Hancock <abel.hancock@gmail.com>
2020-11-30 15:54:49 -0800

    style updates to content component

--- src/pages/2020/components/Content/index.js
+++ src/pages/2020/components/Content/index.js
@@ -2,13 +2,13 @@ import { Heading, Text, Flex } from 'components/atoms'
 import React from 'react'
 import styles from './styles.module.scss'
 
-const Content = ({ pretitle, title, copy, align, justify }) => {
+const Content = ({ pretitle, title, copy, align, justify, inverted, textAlign }) => {
 	return (
-		<Flex align={align} justify={justify} direction="column">
-			<Text weight="black" color="primary" className={styles.preHeader}>
+		<Flex padding="0 0 120px 0" align={align} justify={justify} direction="column" textAlign={textAlign}>
+			<Text weight="regular" color="primary" className={styles.preHeader}>
 				{pretitle}
 			</Text>
-			<Heading className={styles.f2} sx={{ color: 'white' }} level={2}>
+			<Heading className={styles.f2} sx={inverted ? {color: '#1f2123'} : {color: '#93A7DB'}} level={2}>
 				{title}
 			</Heading>
 			{copy &&
