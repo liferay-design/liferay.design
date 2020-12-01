a0d08f791754c78bcfe6dcd447a5254ce405f616
Abel Hancock <abel.hancock@gmail.com>
2020-11-30 16:41:43 -0800

    update hero section and add quote/image section

--- src/pages/2020/index.js
+++ src/pages/2020/index.js
@@ -9,6 +9,7 @@ import { Footer } from 'components/organisms'
 
 import moment from 'moment'
 import MediaQuery from 'react-responsive'
+import Plx from 'react-plx'
 
 import {
 	Nav,
@@ -19,11 +20,119 @@ import {
 import styles from './styles.module.scss'
 import data from 'markdown/annual-reports/TwentyTwenty.yaml'
 
+//   constant plx data
+const InView = [
+	{
+		start: 'self',
+		duration: 1,
+		properties: [],
+	},
+]
+const InView10 = [
+	{
+		start: 'self',
+		duration: 1,
+		startOffset: '10vh',
+		properties: [],
+	},
+]
+const PlxUp1 = [
+	{
+		start: 'self',
+		duration: '200vh',
+		properties: [
+			{
+				startValue: 50,
+				endValue: -50,
+				property: 'translateY',
+			},
+		],
+	},
+]
+const PlxUp2 = [
+	{
+		start: 'self',
+		duration: '200vh',
+		properties: [
+			{
+				startValue: 100,
+				endValue: -100,
+				property: 'translateY',
+			},
+		],
+	},
+]
+const PlxUp3 = [
+	{
+		start: 'self',
+		duration: '200vh',
+		properties: [
+			{
+				startValue: 150,
+				endValue: -150,
+				property: 'translateY',
+			},
+		],
+	},
+]
+const PlxDown1 = [
+	{
+		start: 'self',
+		duration: '200vh',
+		properties: [
+			{
+				startValue: 0,
+				endValue: 20,
+				unit: "vh",
+				property: 'translateY',
+			},
+		],
+	},
+]
+const PlxDown2 = [
+	{
+		start: 'self',
+		duration: '200vh',
+		properties: [
+			{
+				startValue: 0,
+				endValue: 80,
+				unit: "vh",
+				property: 'translateY',
+			},
+			{
+				startValue: 1,
+				endValue: 0,
+				property: 'opacity',
+			}
+		],
+	},
+]
+const PlxDown3 = [
+	{
+		start: 'self',
+		duration: '200vh',
+		properties: [
+			{
+				startValue: 0,
+				endValue: 100,
+				unit: "vh",
+				property: 'translateY',
+			},
+			{
+				startValue: 1,
+				endValue: 0,
+				property: 'opacity',
+			}
+		],
+	},
+]
+
 const TwentyTwenty = () => {
 	return (
 		<div className={styles.pagestyles}>
 			<SEO
-				pageTitle="Liferay Design | 2019 Annual Report"
+				pageTitle="Liferay Design | 2020 Annual Report"
 				description="Highlights, case studies, and more from Communication Design, Product Design, and Design Research at Liferay."
 				previewImage="/images/2019/2019-open-graph.png"
 				twitterHandle="@liferaydesign"
@@ -36,6 +145,8 @@ const TwentyTwenty = () => {
 					type="font/woff2"
 					crossorigin=""
 				/>
+				<link rel="preconnect" href="https://fonts.gstatic.com" />
+				<link href="https://fonts.googleapis.com/css2?family=Bebas+Neue" rel="stylesheet" />
 			</Helmet>
 
 			{/* ----------- SCROLL PROGRESS BAR ---------- */}
@@ -50,22 +161,64 @@ const TwentyTwenty = () => {
 			<Flex
 				as="section"
 				id="top"
-				sx={{ height: '100vh', width: '100vw', flexDirection: 'column' }}
+				sx={{ height: '100vh', width: '100vw', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', margin: '0 auto !important', overflow: 'hidden' }}
 			>
-				<KeyArt />
-				<Heading
-					level={1}
-					sx={{
-						textAlign: 'center',
-						variant: 'text.caps',
-						color: 'mainL3',
-						fontWeight: 'body',
-						letterSpacing: '1em',
-					}}
-				>
-					{data.title}
-				</Heading>
+				<Plx parallaxData={PlxDown2}>
+					<Heading
+						level={1}
+						sx={{
+							textAlign: 'center',
+							variant: 'text.caps',
+							color: '#93A7DB',
+							fontWeight: 'body',
+							letterSpacing: '0.5em',
+							fontFamily: 'Bebas Neue',
+						}}
+					>
+						{data.title}
+					</Heading>
+				</Plx>
+				<Plx parallaxData={PlxDown3} sx={{width: '100%'}}>
+					<KeyArt />
+				</Plx>
 			</Flex>
+
+			{/* ---------- INSPIRATIONAL QUOTE ----------- */}
+			<div style={{background: 'white'}}>
+				<Flex
+					as="section"
+					sx={{
+						flexFlow: 'column-reverse',
+						justifyContent: 'center',
+						alignItems: 'center',
+						textAlign: 'center',
+					}}
+					>
+						<Flex
+							className={styles.heroImages}
+							sx={{
+								left: '23%',
+								position: 'relative',
+								width: '150vw',
+						}}>
+							<Image rounded src="/images/2020/paul-hanaoka-hNQCPAz4ILU-unsplash.jpg"/>	
+							<Image rounded margin="0 4%" src="/images/2020/paul-hanaoka-OTqCo3fdWyw-unsplash.jpg"/>	
+							<Image rounded margin="auto 0 0" src="/images/2020/paul-hanaoka-4yG7Y0rAZvA-unsplash.jpg"/>	
+						</Flex>
+						{/* <img src="/images/2020/unsplash-images.jpg" style={{width: "150vw", maxWidth: '3000px', left: '23%', position: 'relative'}}></img> */}
+						<Plx parallaxData={PlxDown1}>
+							<Content
+								pretitle={data.quote.pretitle}
+								title={data.quote.title}
+								align="center"
+								justify="center"
+								inverted
+								textAlign="center"
+								zIndex="1"
+								/>
+						</Plx>
+				</Flex>
+			</div>
 
 			{/* ---------- TEAM ----------- */}
 			{/* HEADER - TEAM */}
