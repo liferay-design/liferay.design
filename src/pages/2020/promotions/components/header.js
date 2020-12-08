30b75660494a4ddef18109e5893d62ee32435361
plhnk <paul.hanaoka@gmail.com>
2020-12-08 09:19:20 -0800

    animateIn and dropshadow to Header

--- src/pages/2020/promotions/components/header.js
+++ src/pages/2020/promotions/components/header.js
@@ -2,7 +2,7 @@
 
 import { jsx, Flex, Grid, Heading, Box, Text } from 'theme-ui'
 import { useStaticQuery, graphql } from 'gatsby'
-import { Link, Icon, Image, Container } from 'components/atoms'
+import { Link, Icon, Image, Container, AnimateIn } from 'components/atoms'
 import { headshotPath, firstWord } from 'utils'
 
 const { kebabCase } = require(`lodash`)
@@ -44,12 +44,10 @@ export default ({}) => {
 					gridTemplateRows: 'repeat(3, 1fr)',
 					gridColumnGap: '16px',
 					gridRowGap: '16px',
-					mt: 5,
+					alignItems: 'center',
 				}}
 			>
-				<Heading
-					sx={{ color: 'main', fontSize: 6, mb: 4, gridArea: '1 / 2 / 2 / 6' }}
-				>
+				<Heading sx={{ color: 'main', fontSize: 6, gridArea: '1 / 2 / 2 / 6' }}>
 					{promotions.title}
 				</Heading>
 				<Flex
@@ -59,27 +57,43 @@ export default ({}) => {
 						justifyContent: 'space-between',
 					}}
 				>
-					{graphqlData.designers.map(({ name }) => (
-						<Box
+					{graphqlData.designers.map(({ name }, index) => (
+						<AnimateIn
+							delay={`${index}` * 0.1 + 's'}
 							sx={{
 								'&:nth-child(2n+0)': {
 									mt: 4,
 								},
 							}}
 						>
-							<Image
-								sx={{
-									height: '10rem',
-									width: '10rem',
-									borderRadius: '1rem',
-								}}
-								role="presentation"
-								title={name.id}
-								src={headshotPath(name.id)}
-								alt={`${name.id}` + '‘s headshot'}
-							/>
+							<Box sx={{ position: 'relative' }}>
+								<Image
+									sx={{
+										borderRadius: '1rem',
+										position: 'absolute',
+										top: 1,
+										height: '100%',
+										right: '0',
+										left: '0',
+										filter: 'blur(32px)',
+										opacity: 0.8,
+										zIndex: -1,
+									}}
+									src={headshotPath(name.id)}
+								/>
+								<Image
+									sx={{
+										height: '10rem',
+										width: '10rem',
+										borderRadius: '1rem',
+									}}
+									title={name.id}
+									src={headshotPath(name.id)}
+									alt={`${name.id}` + '‘s headshot'}
+								/>
+							</Box>
 							<Text sx={{ color: 'mainL4' }}>{firstWord(name.id)}</Text>
-						</Box>
+						</AnimateIn>
 					))}
 				</Flex>
 				<Heading
@@ -87,9 +101,8 @@ export default ({}) => {
 						gridArea: '3 / 2 / 4 / 6',
 						width: '48ch',
 						lineHeight: '1.5',
-						color: 'mainL2',
+						color: 'mainL3',
 						fontFamily: 'modes.brand.body',
-						mt: 4,
 						fontSize: 4,
 						fontWeight: 'body',
 					}}
