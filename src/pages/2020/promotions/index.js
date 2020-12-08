9ff6be08052cbd484aaf284c7226dd2f6151424a
plhnk <paul.hanaoka@gmail.com>
2020-12-08 00:44:57 -0800

    feature: start promotions page — add page, query, sketch out top

---
+++ src/pages/2020/promotions/index.js
@@ -0,0 +1,46 @@
+/** @jsx jsx */
+
+import { jsx, Grid, Box } from 'theme-ui'
+import Header from './components/header.js'
+import {
+	Flex,
+	Link,
+	Heading,
+	Image,
+	Icon,
+	ScrollProgress,
+	ScrollArrow,
+} from 'components/atoms'
+import { SEO, FancyFooter, FormNewsletter } from 'components/molecules'
+import { Footer } from 'components/organisms'
+
+import data from 'markdown/annual-reports/TwentyTwenty.yaml'
+
+const promotions = data.promotionsPage
+
+const Promotions = () => {
+	return (
+		<div>
+			<SEO
+				pageTitle="Liferay Design | 2020 Promotions"
+				description="Designers at Liferay are exceptional — a few this year earned title changes based on their hard work and in anticipation of expanded responsibility."
+				previewImage=""
+				twitterHandle="@liferaydesign"
+			/>
+
+			{/* ----------- SCROLL PROGRESS BAR ---------- */}
+			<ScrollProgress />
+			<ScrollArrow />
+
+			{/* --------- HERO ---------- */}
+			<Box sx={{background: 'white', height: '100vh'}}>
+				<Header />
+			</Box>
+
+			<Footer />
+			<FancyFooter />
+		</div>
+	)
+}
+
+export default Promotions
