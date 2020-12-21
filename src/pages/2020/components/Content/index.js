43894f4efdd5e13b0a1d353c1b6488c2adcfff3f
plhnk <paul.hanaoka@gmail.com>
2020-12-20 23:20:16 -0800

    chore: finish import of initial content draft

--- src/pages/2020/components/Content/index.js
+++ src/pages/2020/components/Content/index.js
@@ -4,11 +4,21 @@ import styles from './styles.module.scss'
 
 const Content = ({ pretitle, title, copy, align, justify, inverted, textAlign }) => {
 	return (
-		<Flex padding="0 0 120px 0" align={align} justify={justify} direction="column" textAlign={textAlign}>
+		<Flex
+			padding="0 0 120px 0"
+			align={align}
+			justify={justify}
+			direction="column"
+			textAlign={textAlign}
+		>
 			<Text weight="regular" color="primary" className={styles.preHeader}>
 				{pretitle}
 			</Text>
-			<Heading className={styles.f2} sx={inverted ? {color: '#1f2123'} : {color: '#93A7DB'}} level={2}>
+			<Heading
+				className={styles.f2}
+				sx={inverted ? { color: '#1f2123' } : { color: '#93A7DB' }}
+				level={2}
+			>
 				{title}
 			</Heading>
 			{copy &&
