c1eaaa895110c6829b20a6772360f52eeda7aac3
plhnk <paul.hanaoka@gmail.com>
2020-12-22 01:12:04 -0800

    polish, remove unused css

--- src/gatsby-plugin-theme-ui/2020.js
+++ src/gatsby-plugin-theme-ui/2020.js
@@ -2,6 +2,7 @@ import baseTheme from 'gatsby-plugin-theme-ui'
 
 export default {
 	...baseTheme,
+	useBodyStyles: false,
 	colors: {
 		...baseTheme.colors,
 		black: '#1f2123',
@@ -12,6 +13,56 @@ export default {
 		twentyTwentyBlue: '#395DED',
 		lightBlue: '#93A7DB',
 		twentyTwentyYellow: '#FFC90A',
+	},
+	fonts: {
+		...baseTheme.fonts,
+		body: '"Source Sans Pro", Tahoma, "Trebuchet MS", sans-serif',
+		heading: '"Source Sans Pro", Tahoma, "Trebuchet MS", sans-serif',
+		condensed: '"Bebas Neue", "Franklin Gothic Condensed", sans-serif',
+	},
+	fontSizes: [8, 12, 16, 20, 24, 32, 40, 48, 56],
+	fontWeights: {
+		...baseTheme.fontWeights,
+		light: 200,
+		heading: 600,
+		bold: 800,
+	},
+	styles: {
+		...baseTheme.styles,
+		root: {
+			...baseTheme.root,
+			fontFamily: 'body',
+			fontWeight: 'body',
+			color: 'red !important',
+		},
+	},
+	lineHeights: {
+		body: 1.5,
+	},
+	letterSpacings: {
+		heading: 'initial',
+	},
+	text: {
+		...baseTheme.text,
+		preheader: {
+			fontFamily: 'condensed',
+			fontSize: 4,
+			fontWeight: 'body',
+			letterSpacing: '.6ch',
+			lineHeight: 1.5,
+			fontWeight: 300,
+			color: 'primary',
+		},
+		display: {
+			fontSize: 8,
+			fontWeight: 'body',
+		},
+		l1: {
+			fontSize: 7,
+		},
+		l2: {
+			fontSize: 7,
+		},
 	},
 	gradients: {
 		splitBackgroundGradient: 'linear-gradient(0deg, rgba(0,0,0,0) 49.99%, #fff 50%)',
