22680021926832d2554ab90c1903b463d2e8cb28
plhnk <paul.hanaoka@gmail.com>
2020-12-23 18:05:27 -0800

    mobile to :100: — fix timeline

--- src/pages/2020/components/Event/index.js
+++ src/pages/2020/components/Event/index.js
@@ -1,18 +1,12 @@
+/** @jsx jsx */
+
+import { jsx, Box, Flex, Grid, Text, Heading, ThemeProvider } from 'theme-ui'
 import { Link } from 'components/atoms'
 import PropTypes from 'prop-types'
-import React from 'react'
 import Plx from 'react-plx'
 import styles from './styles.module.scss'
-import { Flex } from 'theme-ui'
 
-const Event = ({
-	name,
-	description,
-	startDate,
-	endDate,
-	url,
-	image,
-}) => {
+const Event = ({ name, description, startDate, endDate, url, image }) => {
 	const designerHired = [
 		{
 			start: 'self',
@@ -47,38 +41,76 @@ const Event = ({
 		},
 	]
 
-	const startMonth = new Date(startDate).toLocaleDateString('en-us', {month: 'short', timeZone: 'UTC'})
-	const startDay = new Date(startDate).toLocaleDateString('en-us', {day: '2-digit', timeZone: 'UTC'})
-	const endDay = new Date(endDate).toLocaleDateString('en-us', {day: '2-digit', timeZone: 'UTC'})
+	const startMonth = new Date(startDate).toLocaleDateString('en-us', {
+		month: 'short',
+		timeZone: 'UTC',
+	})
+	const startDay = new Date(startDate).toLocaleDateString('en-us', {
+		day: '2-digit',
+		timeZone: 'UTC',
+	})
+	const endDay = new Date(endDate).toLocaleDateString('en-us', {
+		day: '2-digit',
+		timeZone: 'UTC',
+	})
 
 	return (
-		<Plx parallaxData={designerHired} className={styles.hired}>
-			<div className={styles.date}>
-				<div className={styles.month}>{startMonth}</div>
+		<Plx parallaxData={designerHired}>
+			<Flex
+				sx={{
+					justifyContent: 'flex-start',
+					alignItems: 'center',
+					position: 'relative',
+					pb: ['20vh', '6vw'],
+					m: '4vw auto',
+					flexDirection: ['column', 'row'],
+				}}
+			>
 				<Flex
 					sx={{
-						justifyContent: 'space-between',
+						flexDirection: 'column',
 						alignItems: 'center',
-					}}>
-					<div className={styles.day}>{startDay}</div>
-					{endDate ? '\u00A0\u00A0—' : null}
-					<div className={styles.day}>{endDate ? endDay : null}</div>
+						justifyContent: 'center',
+						width: '6rem',
+						height: '6rem',
+						variant: 'boxes.icon',
+						textAlign: 'center',
+					}}
+					className={styles.date}
+				>
+					<div className={styles.month}>{startMonth}</div>
+					<Flex
+						sx={{
+							justifyContent: 'center',
+							alignItems: 'center',
+						}}
+					>
+						<div className={styles.day}>{startDay}</div>
+						{endDate ? '\u00A0\u00A0—' : null}
+						<div className={styles.day}>{endDate ? endDay : null}</div>
+					</Flex>
 				</Flex>
-			</div>
-			<Link to={url}>
-				<Plx
-					tagName="img"
-					parallaxData={eventImage}
-					className={styles.image}
-					src={image}
-				/>
-			</Link>
-			<h4 className={styles.pLarge}>
-				{name}
-				<span>
-					{description}
-				</span>
-			</h4>
+				<Flex
+					sx={{
+						flexDirection: ['column', 'row'],
+						my: [4, 0],
+						alignItems: 'center',
+					}}
+				>
+					<Link to={url} sx={{ mx: 4, mt: [4, 0], order: 2 }}>
+						<Plx
+							tagName="img"
+							parallaxData={eventImage}
+							className={styles.image}
+							src={image}
+						/>
+					</Link>
+					<h4 className={styles.pLarge}>
+						{name}
+						<span>{description}</span>
+					</h4>
+				</Flex>
+			</Flex>
 		</Plx>
 	)
 }
