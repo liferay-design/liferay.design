dd3eb8251a01307fb02123c31bd745e68a4da8d5
plhnk <paul.hanaoka@gmail.com>
2021-04-15 23:42:30 -0700

    add Remote option for /careers

--- src/pages/careers.js
+++ src/pages/careers.js
@@ -7,6 +7,8 @@ import { MainLayout } from 'components/templates'
 import { graphql } from 'gatsby'
 
 export default ({ data }) => {
+	console.log(data.allMdx.edges, 'data')
+
 	return (
 		<MainLayout section="Careers">
 			<SEO
@@ -22,12 +24,29 @@ export default ({ data }) => {
 						<CardDefault
 							delay={`${index}` * 0.1 + 's'}
 							avatarImage
+							pill
+							tag={node.frontmatter.remote === true ? 'Remote Available 😊' : null}
 							key={node.id}
 							imageURL={node.frontmatter.featuredImage}
 							link={node.fields.slug}
 							title={node.frontmatter.title}
-							subtitle={node.frontmatter.office.city}
-							avatarImageURL={node.frontmatter.office.regionIcon}
+							// subtitle={
+							// 	node.frontmatter.remote === true
+							// 		? node.frontmatter.office.region.countries.map(
+							// 				(i, index) => (index ? ' / ' : '') + i,
+							// 		  )
+							// 		: node.frontmatter.office.city
+							// }
+							subtitle={
+								node.frontmatter.remote === true
+									? node.frontmatter.office.country
+									: node.frontmatter.office.city
+							}
+							avatarImageURL={
+								node.frontmatter.remote === true
+									? node.frontmatter.office.countryIcon
+									: node.frontmatter.office.countryIcon
+							}
 						/>
 					))}
 				</Grid>
@@ -53,9 +72,15 @@ export const query = graphql`
 						title
 						office {
 							city
-							regionIcon
+							region {
+								countries
+								countryIcon
+							}
+							country
+							countryIcon
 						}
 						featuredImage
+						remote
 					}
 					fields {
 						slug
