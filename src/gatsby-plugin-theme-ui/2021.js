0b19f0f570bcc2e6baedfc8fc014747bdbf923de
plhnk <paul.hanaoka@gmail.com>
2021-12-18 12:06:00 -0800

    2020 -> 2021, create 2021 theme

---
+++ src/gatsby-plugin-theme-ui/2021.js
@@ -0,0 +1,108 @@
+import baseTheme from 'gatsby-plugin-theme-ui'
+
+export default {
+	...baseTheme,
+	useBodyStyles: false,
+	colors: {
+		...baseTheme.colors,
+		black: '#1f2123',
+		darkBlack: '#1b1c1e',
+		lightBlack: '#1f2123',
+		grayHeader: '#555e68',
+		grayParagraph: '#888b9b',
+		twentyTwentyBlue: '#395DED',
+		lightBlue: '#93A7DB',
+		twentyTwentyYellow: '#FFC90A',
+	},
+	fonts: {
+		...baseTheme.fonts,
+		body: '"Source Sans Pro", Tahoma, "Trebuchet MS", sans-serif',
+		heading: '"Source Sans Pro", Tahoma, "Trebuchet MS", sans-serif',
+		condensed: '"Bebas Neue", "Franklin Gothic Condensed", sans-serif',
+	},
+	fontSizes: [8, 12, 16, 20, 24, 32, 40, 48, 56],
+	fontWeights: {
+		...baseTheme.fontWeights,
+		light: 200,
+		heading: 600,
+		bold: 800,
+	},
+	lineHeights: {
+		body: 1.5,
+	},
+	letterSpacings: {
+		heading: 'initial',
+	},
+	boxes: {
+		icon: {
+			display: 'inline-block',
+			padding: '1rem',
+			borderRadius: '1rem',
+			background: theme => `${theme.gradients.backgroundGradient}`,
+			boxShadow: '0 8px 24px -4px rgba(27,28,30,0.6)',
+			'> svg': {
+				display: 'block',
+				height: '3rem',
+				width: '3rem',
+				color: 'primary',
+			},
+		},
+		section: {
+			width: '100vw',
+			margin: 'auto',
+			maxWidth: '1200px',
+			position: 'relative',
+		},
+		gridTwoCol: {
+			mx: [2, 'auto', null],
+			mt: [2, 5],
+			maxWidth: '1080px',
+			gridTemplateColumns: ['1fr', '480px 1fr 480px'],
+			gridTemplateRows: 'auto',
+			gridColumnGap: 0,
+			gridRowGap: 0,
+		},
+	},
+	text: {
+		...baseTheme.text,
+		preheader: {
+			fontFamily: 'condensed',
+			fontSize: 4,
+			fontWeight: 'body',
+			letterSpacing: '.6ch',
+			lineHeight: 1.5,
+			fontWeight: 300,
+			color: 'primary',
+		},
+		display: {
+			fontSize: [6, 8],
+			fontWeight: 'body',
+		},
+		l1: {
+			fontSize: 7,
+		},
+		l2: {
+			fontSize: [5, 7],
+		},
+		l3: {
+			fontSize: 6,
+			fontWeight: 'bold',
+		},
+		pLarge: {
+			fontSize: [3, 4],
+			fontWeight: 'light',
+			mb: 2,
+		},
+		pMedium: {
+			fontSize: 3,
+			fontWeight: 'light',
+			mb: 2,
+		},
+	},
+	gradients: {
+		splitBackgroundGradient: 'linear-gradient(0deg, rgba(0,0,0,0) 49.99%, #fff 50%)',
+		backgroundGradient: 'linear-gradient(247.41deg, #212B32 6.16%, #1C1F23 63.57%)',
+		lightBackgroundGradient:
+			'linear-gradient(219.58deg, rgba(61, 73, 86, 0.8) -2.98%, rgba(66, 81, 98, 0) 79.84%), linear-gradient(0deg, #232A31, #232A31)',
+	},
+}
--- src/pages/2021/promotions/index.js
+++ src/pages/2021/promotions/index.js
@@ -1,47 +1,60 @@
 /** @jsx jsx */
 
-import { jsx } from 'theme-ui'
+import { jsx, ThemeProvider } from 'theme-ui'
+import theme from 'gatsby-plugin-theme-ui/2021'
 import Header from './components/header.js'
 import Footer from './components/footer.js'
 import Designers from './components/designerSection.js'
 import { Link, Icon, ScrollArrow } from 'components/atoms'
 import { SEO } from 'components/molecules'
+import { Global } from '@emotion/core'
 
 const Promotions = () => {
 	return (
-		<div sx={{ fontFamily: 'modes.brand.body', color: 'mainL3' }}>
-			<SEO
-				pageTitle="Liferay Design | 2020 Promotions"
-				description="Designers at Liferay are exceptional — a few this year earned title changes based on their hard work and in anticipation of expanded responsibility."
-				previewImage="/images/2020/promotions-og.png"
-				twitterHandle="@liferaydesign"
+		<ThemeProvider theme={theme}>
+			<Global
+				styles={theme => ({
+					body: {
+						fontFamily: theme.fonts.body,
+						color: theme.colors.lightBlue,
+						lineHeight: theme.lineHeights.body,
+					},
+				})}
 			/>
+			<div sx={{ fontFamily: 'modes.brand.body', color: 'mainL3' }}>
+				<SEO
+					pageTitle="Liferay Design | 2021 Promotions"
+					description="Designers at Liferay are exceptional — a few this year earned title changes based on their hard work and in anticipation of expanded responsibility."
+					previewImage="/images/2020/promotions-og.png" // TODO
+					twitterHandle="@liferaydesign"
+				/>
 
-			<Link
-				sx={{
-					variant: 'links.nice',
-					color: 'white',
-					top: '2rem',
-				}}
-				to="/2020"
-			>
-				Go to 2020 Report
-				<Icon sx={{ m: '0 1em' }} name="rightArrow" />
-			</Link>
+				<Link
+					sx={{
+						variant: 'links.nice',
+						color: 'white',
+						top: '2rem',
+					}}
+					to="/2021"
+				>
+					Go to 2021 Report
+					<Icon sx={{ m: '0 1em' }} name="rightArrow" />
+				</Link>
 
-			<ScrollArrow
-				sx={{
-					left: ['initial', '2rem', null],
-					right: ['2rem', 'initial', null],
-				}}
-			/>
+				<ScrollArrow
+					sx={{
+						left: ['initial', '2rem', null],
+						right: ['2rem', 'initial', null],
+					}}
+				/>
 
-			<Header />
+				<Header />
 
-			<Designers />
+				<Designers />
 
-			<Footer />
-		</div>
+				<Footer />
+			</div>
+		</ThemeProvider>
 	)
 }
 
