740871091f12bed5d5ff943bb2eceb957f917336
plhnk <paul.hanaoka@gmail.com>
2021-12-20 10:38:54 -0800

    add real redirect

--- src/pages/2021/report.js
+++ src/pages/2021/report.js
@@ -4,7 +4,7 @@ import React from 'react'
 
 export default class TwentyTwentyOne extends React.Component {
 	componentDidMount() {
-		window.location.replace('https://www.abelhancock.com/contact')
+		window.location.replace('https://2021.liferay.design')
 	}
 	render() {
 		return null
